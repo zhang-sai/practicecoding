616c8a6f6a14a0fe99a813f528d5791f6c165d63
racezhang <racezhang@gmail.com>
2013-02-08 18:10:26 +0000

    rotate array search

--- leetcode/src/SearchInRotateArray.java
+++ leetcode/src/SearchInRotateArray.java
@@ -10,11 +10,51 @@ You may assume no duplicate exists in the array.
 
 » Solve this problem
  * */
+//XXX for binary search, use the recursive form
 public class SearchInRotateArray {
 
-	public int search(int[] A, int target) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        
+	//suppose the array is sorted in increasing order
+	public int search(int[] a, int target) {
+        return this.findTarget(a, target, 0, a.length - 1);
     }
+	
+	public int findTarget(int[] a, int target, int startIndex, int endIndex) {
+		if(a.length == 0) {
+        	return -1;
+        }
+		if(startIndex > endIndex) {
+			return -1;
+		}
+		int midIndex = (startIndex + endIndex)/2;
+		if(a[midIndex] == target) {
+			return midIndex;
+		} else {
+    		//decide which part should search
+    		//here is some possibilities
+    		if(a[startIndex] < a[endIndex]) {
+    			//the array is sorted, the normal binary search
+    			if(a[midIndex] > target) {
+    				return this.findTarget(a, target, startIndex, midIndex - 1);
+    			} else {
+    				return this.findTarget(a, target, midIndex + 1, endIndex);
+    			}
+    		} else {
+    			//XXX in this case there are always 2 possibility
+    			//3, 5, 1 ==> find 3
+    			//3, 5, 1 ==> find 1
+    			int index1 = this.findTarget(a, target, startIndex, midIndex - 1);
+    			if(index1 == -1) {
+				    int index2 = this.findTarget(a, target, midIndex + 1, endIndex);
+				    return index2;
+    			} else {
+    				return index1;
+    			}
+    		}
+    	}
+	}
+	
+	public static void main(String[] args) {
+		SearchInRotateArray s = new SearchInRotateArray();
+		System.out.println(s.search(new int[]{3, 5, 1}, 3));
+	}
 }
