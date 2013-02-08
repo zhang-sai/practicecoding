0587d71d5882a04997e6abade5f9032c04717f55
racezhang <racezhang@gmail.com>
2013-02-08 18:12:01 +0000

    binary search

--- leetcode/src/BinarySearch.java
+++ leetcode/src/BinarySearch.java
@@ -1,5 +1,26 @@
 
 public class BinarySearch {
 
-	xx
+	public int binary_search(int A[], int key, int imin, int imax)
+	{
+	  // continue searching while [imin,imax] is not empty
+	  while (imax >= imin)
+	    {
+	      /* calculate the midpoint for roughly equal partition */
+	      int imid = (imin + imax)/2;
+	 
+	      // determine which subarray to search
+	      if (A[imid] < key)
+	        // change min index to search upper subarray
+	        imin = imid + 1;
+	      else if (A[imid] > key)
+	        // change max index to search lower subarray
+	        imax = imid - 1;
+	      else
+	        // key found at index imid
+	        return imid;
+	    }
+	  // key not found
+	  return -1;
+	}
 }
