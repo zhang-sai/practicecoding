31fb721f9a5216e1a4e3fa738cc95b6311615457
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-02-09 19:20:01 +0000

    add new problems

---
+++ trunk/leetcode/src/LongestValidParenth.java
@@ -0,0 +1,15 @@
+
+/**
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+ * */
+public class LongestValidParenth {
+	 public int longestValidParentheses(String s) {
+	        // Start typing your Java solution below
+	        // DO NOT write main() function
+	        
+	    }
+}
