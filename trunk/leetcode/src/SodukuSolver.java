f1395c7a5e53cc52dafaa46ad4ab9be33d866b6a
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-02-12 04:01:11 +0000

    updated coding practices

---
+++ trunk/leetcode/src/SodukuSolver.java
@@ -0,0 +1,12 @@
+/**
+ * Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+ * */
+public class SodukuSolver {
+	public boolean isValidSudoku(char[][] board) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+}
