69b005c8f06dda7517547e977a0482115bc6a512
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-02-16 20:05:40 +0000

    updates

---
+++ trunk/leetcode/src/MaxSumInMatrix.java
@@ -0,0 +1,10 @@
+
+/**
+ * Pick up an element from each different row and different line, and
+ * get the max sum value
+ * */
+public class MaxSumInMatrix {
+	
+	xxx
+	
+}
---
+++ trunk/leetcode/src/Subsets.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+/**
+ * Given a set of distinct integers, S, return all possible subsets.
+
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+» Solve this problem
+ * */
+public class Subsets {
+	public ArrayList<ArrayList<Integer>> subsets(int[] s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+		list.add(new ArrayList<Integer>());
+        int size = s.length;
+        if(size == 0) {
+        	return list;
+        }
+        //number of possible non-empty subsets
+        int num = (int)Math.pow(2.0d, (double)size) - 1;
+        
+        for(int k = 1; k <= num; k++) {
+        	//convert it into an arraylist
+        	ArrayList<Integer> set  = new ArrayList<Integer>();
+        	int value = k;
+        	int index = 0;
+        	int mod = value%2;
+        	if(mod == 1) {
+        		set.add(s[index]);
+        	}
+        	while(value/2 != 0) {
+        		index++;
+        		value = value/2;
+        		mod = value%2;
+        		if(mod == 1) {
+            		set.add(s[index]);
+            	}
+        	}
+        	
+        	Collections.sort(set);
+        	list.add(set);
+        }
+        
+        return list;
+    }
+	
+	public static void main(String[] args) {
+		Subsets as = new Subsets();
+		int[] set = new int[]{4, 1, 0};
+		System.out.println(as.subsets(set));
+	}
+}
