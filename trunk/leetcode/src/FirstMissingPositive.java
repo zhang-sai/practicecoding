3e6b348ada71a9b42ba993692c5ac5d3c77a6d61
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-02-18 07:06:07 +0000

    a sample answer

--- trunk/leetcode/src/FirstMissingPositive.java
+++ trunk/leetcode/src/FirstMissingPositive.java
@@ -9,38 +9,36 @@ and [3,4,-1,1] return 2.
 
 Your algorithm should run in O(n) time and uses constant space.
  * */
-XXXX may have duplication
+//XXX may have duplication
 public class FirstMissingPositive {
+	/**
+	 * Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+	 * */
+	//XXX a fairly smart solution
+	//a sample solution here: http://yewenxing.wordpress.com/2012/03/13/first-missing-positive/
 	public int firstMissingPositive(int[] a) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		if(a.length == 0) {
-			return 1;
-		}
-		
-		int sum = 0;
-		for(int i : a) {
-			if(i <= 0) {
-				continue;
-			}
-			int pow = (int)Math.pow(2.0, (double)i);
-			sum = sum + pow;
-		}
-		
-		//decide which positive is missing
-		if(sum == 0) {
-			return 1;
-		}
-        sum = sum/2; //remove the last c
-        int missing = 1;
-        while(sum != 0) {
-        	int mod = sum%2;
-        	if(mod == 0) {
-        		return missing;
+        int length = a.length;
+        //put every element in A such that A[i]=i
+        for(int i = 0; i < length; i++) {
+        	while(a[i] != i+1) { //XXX note this is a while loop until a[i] == i + 1
+        		int value = a[i];
+        	    if(value <= 0 || value > length || value == a[value -1]) {
+        	    	break;
+        	    }
+        	    int tmp = value;
+        	    a[i] = a[value -1];
+        	    a[value -1] = tmp;
         	}
-        	sum = sum/2;
-        	missing++;
         }
-        return missing;
+        for(int i=0;i<length;i++){
+            if(a[i]!=i+1)   return i+1;
+        }
+        return length+1;
     }
 }
