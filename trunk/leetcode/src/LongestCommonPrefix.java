4e8056454e8149f20cbd0b1051f20caeb6f5d27b
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-02-18 07:30:09 +0000

    combinations

---
+++ trunk/leetcode/src/LongestCommonPrefix.java
@@ -0,0 +1,12 @@
+
+public class LongestCommonPrefix {
+
+	/**
+	 * Write a function to find the longest common prefix string amongst an array of strings.
+	 * */
+	public String longestCommonPrefix(String[] strs) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+}
