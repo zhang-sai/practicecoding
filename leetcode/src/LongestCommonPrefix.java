34f5fba11f9bd789a6c672478ab305bcee325746
racezhang <racezhang@gmail.com>
2013-02-20 05:59:11 +0000

    longest common prefix

--- leetcode/src/LongestCommonPrefix.java
+++ leetcode/src/LongestCommonPrefix.java
@@ -7,6 +7,34 @@ public class LongestCommonPrefix {
 	public String longestCommonPrefix(String[] strs) {
         // Start typing your Java solution below
         // DO NOT write main() function
+		if(strs.length == 0) {
+			return "";
+		}
+		
+		for(String str : strs) {
+			if(str.isEmpty()) {
+				return "";
+			}
+		}
         
+		String prefix = strs[0];
+		for(int i = 1; i < strs.length; i++) {
+			String nextString = strs[i];
+			
+			int index = 0;
+			for(index = 0; index < prefix.length() && index < nextString.length(); index++) {
+				if(nextString.charAt(index) != prefix.charAt(index)) {
+					break;
+				}
+			}
+			
+			if(index == 0) {
+				return "";
+			}
+			prefix = prefix.substring(0, index);
+			
+		}
+		
+		return prefix;
     }
 }
