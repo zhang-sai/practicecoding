c7378ae696b06ac0ed998ddc69f172e01d5187c0
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-02-24 19:42:17 +0000

    binary tree level traversal

---
+++ trunk/leetcode/src/BinaryTreeLevelOrder.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+/**
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+ * */
+public class BinaryTreeLevelOrder {
+	public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+		ArrayList<ArrayList<Integer>> retList = new ArrayList<ArrayList<Integer>>();
+		if(root == null) {
+			return retList;
+		}
+        ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+        list.add(root);
+        while(!list.isEmpty()) {
+        	ArrayList<Integer> iList = new ArrayList<Integer>();
+        	for(TreeNode n : list) {
+        		iList.add(n.val);
+        	}
+        	retList.add(iList);
+        	list = this.getNextLevel(list);
+        }
+        return retList;
+    }
+	
+	ArrayList<TreeNode> getNextLevel(ArrayList<TreeNode> currLevel) {
+		ArrayList<TreeNode> nodeList = new ArrayList<TreeNode>();
+		for(TreeNode node : currLevel) {
+			if(node.left != null) {
+				nodeList.add(node.left);
+			}
+			if(node.right != null) {
+				nodeList.add(node.right);
+			}
+		}
+		return nodeList;
+	}
+}
