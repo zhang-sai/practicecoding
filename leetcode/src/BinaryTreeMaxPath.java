bee9cacf1d1c0dac5c77640065534b2ce673f59a
racezhang <racezhang@gmail.com>
2013-02-24 06:55:04 +0000

    node

--- leetcode/src/BinaryTreeMaxPath.java
+++ leetcode/src/BinaryTreeMaxPath.java
@@ -57,11 +57,3 @@ public class BinaryTreeMaxPath {
 		System.out.println(t);
 	}
 }
-
-
- class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
-}
