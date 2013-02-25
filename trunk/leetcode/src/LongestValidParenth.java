bc1fe837ad26c8f4d4e0927451e93c8f4628713a
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-02-25 18:54:50 +0000

    add some tricks to make binary tree balance problem work

--- trunk/leetcode/src/LongestValidParenth.java
+++ trunk/leetcode/src/LongestValidParenth.java
@@ -16,47 +16,14 @@ public class LongestValidParenth {
 		 char left = '(';
 		 char right = ')';
 		 
-		 boolean[][] isValid = new boolean[s.length()][s.length()];
-		 int[][] longest = new int[s.length()][s.length()];
-		 
-		 for(int i = 0; i < s.length(); i++) {
-			 int j = i + 1;
-			 if((cs[i] == left && cs[j] == right) || (cs[j] == left && cs[i] == right)) {
-				 isValid[i][j] = true;
-				 longest[i][j] = 2;
-			 } else {
-				 isValid[i][j] = false;
-				 longest[i][j] = 0;
+		 int maxLength = 0;
+		 for(int i = 0; i < cs.length - 1; i++) {
+			 if(cs[i] == left && cs[i+1] == right) {
+				 //expand from here
+				 
+				 int iter = Math.min(i, cs.length - )
 			 }
 		 }
-		 
-		 for(int i = 0; i < s.length(); i++) {
-			 for(int j = i + 2; j < s.length(); j++) {
-				 //three possibilities
-				 if(j > i + 2) {
-					 int max = Integer.MIN_VALUE;
-					 if(isValid[i][j-2] && ((cs[j] == left && cs[j-1] == right) || (cs[j-1] == left && cs[j] == right))) {
-						 int length = longest[i][j-2] + 2;
-						 if(length > max) {
-							 max = length;
-							 isValid[i][j] = true;
-						 }
-					 } else if(isValid[i+2][j] && ((cs[i] == left && cs[i+1] == right) || (cs[i+1] == left && cs[i] == right))) {
-						 int length = longest[i][j-2] + 2;
-						 if(length > max) {
-							 max = length;
-							 isValid[i][j] = true;
-						 }
-					 } else if {
-						 
-					 }
-				 } else{
-					 
-				 }
-			 }
-		 }
-		 
-		 return longest[0][s.length() - 1];
 	        
 	 }
 }
