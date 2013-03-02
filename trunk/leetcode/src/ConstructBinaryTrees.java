45085ac3934c348733f04ce994817aa1e17f03ff
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-03-02 20:22:39 +0000

    new questions

---
+++ trunk/leetcode/src/ConstructBinaryTrees.java
@@ -0,0 +1,28 @@
+/**
+ * */
+public class ConstructBinaryTrees {
+
+	/**
+	 * Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+	 * */
+	public TreeNode buildTree1(int[] inorder, int[] postorder) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+	
+	/**
+	 * Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+	 * */
+	 public TreeNode buildTree2(int[] preorder, int[] inorder) {
+	        // Start typing your Java solution below
+	        // DO NOT write main() function
+	        
+	 }
+}
---
+++ trunk/leetcode/src/RomaToIntegerToRoma.java
@@ -0,0 +1,24 @@
+
+public class RomaToIntegerToRoma {
+	/**
+	 * Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+	 * */
+	public int romanToInt(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+	
+	/**
+	 * Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+	 * */
+	public String intToRoman(int num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+}
