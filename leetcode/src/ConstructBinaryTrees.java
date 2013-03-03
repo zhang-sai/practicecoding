71af6dd32616f6d2d05c98584cb5311cc9ca490e
racezhang <racezhang@gmail.com>
2013-03-03 00:11:47 +0000

    add new questions

--- leetcode/src/ConstructBinaryTrees.java
+++ leetcode/src/ConstructBinaryTrees.java
@@ -8,6 +8,7 @@ public class ConstructBinaryTrees {
 Note:
 You may assume that duplicates do not exist in the tree.
 	 * */
+	//http://leetcode.com/2011/04/construct-binary-tree-from-inorder-and-preorder-postorder-traversal.html
 	public TreeNode buildTree1(int[] inorder, int[] postorder) {
         // Start typing your Java solution below
         // DO NOT write main() function
--- leetcode/src/SpiralMatrix.java
+++ leetcode/src/SpiralMatrix.java
@@ -19,4 +19,23 @@ public class SpiralMatrix {
         // DO NOT write main() function
         
     }
+	
+	/**
+	 * Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+For example,
+Given n = 3,
+
+You should return the following matrix:
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
+	 * */
+	public int[][] generateMatrix(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
 }
