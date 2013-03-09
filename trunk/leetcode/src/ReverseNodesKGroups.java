4f67fdba7f5c5f6e4bf95d93f14778a4c75d0a5d
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-03-09 18:58:28 +0000

    two more questions

---
+++ trunk/leetcode/src/ReverseNodesKGroups.java
@@ -0,0 +1,25 @@
+/**
+ * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+ * */
+public class ReverseNodesKGroups {
+	public ListNode reverseKGroup(ListNode head, int k) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        xxx
+        
+        return newHead;
+    }
+}
