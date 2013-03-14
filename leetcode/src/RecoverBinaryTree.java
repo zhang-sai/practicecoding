a868e5a894e7fc32a27fe87c470ce5506b0b9a58
racezhang <racezhang@gmail.com>
2013-03-14 21:30:28 +0000

    recover binary tree

--- leetcode/src/RecoverBinaryTree.java
+++ leetcode/src/RecoverBinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Two elements of a binary search tree (BST) are swapped by mistake.
 
@@ -8,30 +11,63 @@ A solution using O(n) space is pretty straight forward. Could you devise a const
 XXX
  * */
 
-xxx
+//one sample solution: https://gist.github.com/xiaonanz/4013895
+//XX
 public class RecoverBinaryTree {
+	//the key point is that the in order traversal of a binary
+	//tree must be a sorted list
 	public void recoverTree(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if(root == null) {
-        	return;
-        }
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        if(left != null && right != null) {
-        	if(right.val < left.val) {
-        		//find and return
-        		root.left = right;
-        		root.right = left;
-        		return;
-        	}
-        } else {
-        	if(left != null) {
-        		this.recoverTree(left);
-        	}
-        	if(right != null) {
-        		this.recoverTree(right);
-        	}
-        }
+		if(root == null) {
+			return;
+		}
+		List<TreeNode> list = this.inorder(root);
+		System.out.println(list);
+		TreeNode n1 = null;
+		TreeNode n2 = null;
+		for(int i = 0; i < list.size() - 1; i++) {
+			if(list.get(i).val > list.get(i + 1).val) {
+				n1 = list.get(i);
+				break; //XXX do not forget break; other wise will be in
+				//trouble, e.g., 3, 2, 1  ==> 3, and 1 are swapped
+			}
+		}
+		for(int i = list.size() - 1; i >0; i --) {
+			if(list.get(i).val < list.get(i - 1).val) {
+				n2 = list.get(i);
+				break;
+			}
+		}
+		
+		System.out.println(n1);
+		System.out.println(n2);
+		
+		int tmp = n1.val;
+		n1.val = n2.val;
+		n2.val = tmp;
+        
     }
+	
+	List<TreeNode> inorder(TreeNode node) {
+		List<TreeNode> arrayList = new ArrayList<TreeNode>();
+		if(node.left != null) {
+			arrayList.addAll(this.inorder(node.left));
+		}
+		arrayList.add(node);
+		if(node.right != null) {
+			arrayList.addAll(this.inorder(node.right));
+		}
+		return arrayList;
+	}
+	
+	public static void main(String[] args) {
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(3);
+		TreeNode n1 = new TreeNode(1);
+		n2.left = n3;
+		n2.right = n1;
+		RecoverBinaryTree rbt = new RecoverBinaryTree();
+		rbt.recoverTree(n2);
+		
+		System.out.println(n2.val);
+	}
 }
