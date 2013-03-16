a49823e25c68b49e055c9b2b445540ca9229c275
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-03-16 23:29:35 +0000

    permutation questions

--- trunk/leetcode/src/Permutations1and2.java
+++ trunk/leetcode/src/Permutations1and2.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Permutations1and2 {
 
@@ -9,6 +13,7 @@ For example,
 [1,2,3] have the following permutations:
 [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
 	 * */
+//	XXX
 	public ArrayList<ArrayList<Integer>> permute(int[] num) {
         // Start typing your Java solution below
         // DO NOT write main() function
@@ -47,11 +52,6 @@ For example,
 		return ret;
 	}
 	
-	public static void main(String[] args) {
-		Permutations1and2 p = new Permutations1and2();
-		System.out.println(p.permute(new int[]{1, 2, 3}));
-	}
-	
 	/**
 	 * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
@@ -59,10 +59,70 @@ For example,
 [1,1,2] have the following unique permutations:
 [1,1,2], [1,2,1], and [2,1,1].
 	 * */
-	
+	//credit goes to: http://www.tsechung.com/wordpress/tag/permutation/
 	public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        return null;
-    }
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		ArrayList<ArrayList<Integer>> al = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> head = new ArrayList<Integer>();
+		Arrays.sort(num);
+		
+		//after sorting the array, the sorted one is the first permutated item
+		for (int i = 0; i < num.length; i++)
+			head.add(num[i]);
+		al.add(head);
+
+		//the total number of possible permutation number
+		int factorial = 1;
+		for (int i = 0; i < num.length; i++)
+			factorial *= (i + 1);
+
+		
+		for (int t = 1; t < factorial; t++) {
+			int first_non_decreasing_index = -1, swap_point = -1;
+
+			// from the end, find first index that num[index]<num[index+1]
+			for (int i = num.length - 1; i > 0; i--) {
+				if (num[i - 1] < num[i]) {
+					first_non_decreasing_index = i - 1;
+					break;
+				} else if (i == 1) {
+					//at this point: num is sorted in a descreasing order
+					return al;
+				}
+			}
+
+			// finding the first numthat num[swap_point]>num[index]
+			for (int i = num.length - 1; i > first_non_decreasing_index; i--) {
+				if (num[first_non_decreasing_index] < num[i]) {
+					swap_point = i;
+					break;
+				}
+			}
+			//swap num[index], num[swap_point]
+			int tmp = num[first_non_decreasing_index];
+			num[first_non_decreasing_index] = num[swap_point];
+			num[swap_point] = tmp;
+
+			// swap the index+1...end sequences
+			for (int i = 0; i < (num.length - 1 - first_non_decreasing_index) / 2; i++) {
+				tmp = num[first_non_decreasing_index + 1 + i];
+				num[first_non_decreasing_index + 1 + i] = num[num.length - 1 - i];
+				num[num.length - 1 - i] = tmp;
+			}
+
+			//create the next permutated list
+			ArrayList<Integer> next = new ArrayList<Integer>();
+			for (int i = 0; i < num.length; i++)
+				next.add(num[i]);
+			al.add(next);
+		}
+		return al;
+	}
+
+	
+	public static void main(String[] args) {
+		Permutations1and2 p = new Permutations1and2();
+		System.out.println(p.permute(new int[]{1, 2, 3}));
+	}
 }
