b89ccd3b2c5ca71348b0ac2751349d673a2b5d75
racezhang <racezhang@gmail.com>
2013-03-25 05:13:14 +0000

    edit

---
+++ leetcode/src/SubstringWithAllWords.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+/**
+ * You are given a string, S, and a list of words, L, that are all of the same length. Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
+
+For example, given:
+S: "barfoothefoobarman"
+L: ["foo", "bar"]
+
+You should return the indices: [0,9].
+(order does not matter).
+ * */
+public class SubstringWithAllWords {
+	public ArrayList<Integer> findSubstring(String S, String[] L) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+}
