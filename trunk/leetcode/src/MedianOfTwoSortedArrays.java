32cb2ccb49ec58884023caea89417ca543de664c
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-03-30 22:26:25 +0000

    unupdate

--- trunk/leetcode/src/MedianOfTwoSortedArrays.java
+++ trunk/leetcode/src/MedianOfTwoSortedArrays.java
@@ -1,13 +1,16 @@
 /**
  * There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
  * */
-xx
+
 //http://leetcode.com/2011/03/median-of-two-sorted-arrays.html
+xx
 public class MedianOfTwoSortedArrays {
 	public static void main(String[] args) {
 		MedianOfTwoSortedArrays m = new MedianOfTwoSortedArrays();
 //		System.out.println(m.findMedianSortedArrays(new int[]{1,2,3}, new int[]{1, 2}));
-		System.out.println(m.findMedianSortedArrays(new int[]{1,2,3}, new int[]{4, 5, 6}));
+//		System.out.println(m.findMedianSortedArrays(new int[]{1,2,3}, new int[]{4, 5, 6}));
+//		System.out.println(m.findMedianSortedArrays(new int[]{3,4}, new int[]{}));
+		System.out.println(m.findMedianSortedArrays(new int[]{2, 3,4}, new int[]{1}));
 	}
 	public double findMedianSortedArrays(int a[], int b[]) {
         // Start typing your Java solution below
@@ -28,30 +31,25 @@ public class MedianOfTwoSortedArrays {
 	//
 	private double findMedianSortedArrays(int a[], int aStart, int aEnd,
 			int b[], int bStart, int bEnd) {
-		System.out.println(aStart + ", " + aEnd + ": " + bStart + ", " + bEnd);
-		c++;
+//		System.out.println(aStart + ", " + aEnd + ": " + bStart + ", " + bEnd);
+//		c++;
 //		if(c > 10) {
 //			return -0d;
 //		}
 		if(bEnd - bStart == 1 && aEnd - aStart == 1) {
-			if(a[aEnd] <= b[bStart]) {
-				return (a[aEnd] + b[bStart]) / 2;
-			} else if (b[bEnd] <= a[aStart]) {
-				return (b[bEnd] + a[aStart]) /2;
-			} 
-			else {
-				
-			}
-			return  a[aStart] >= b[bEnd] ? (a[aStart] + b[bEnd])/2 : (a[aEnd] + b[bStart])/2;
+			int sum = a[aStart] + a[aEnd] + b[bStart] + b[bEnd];
+			return (double)(sum - Math.min(a[aStart], b[bStart]) - Math.max(a[aEnd], b[bEnd]))/2;
 		}
 		if(aStart == aEnd && bStart == bEnd) {
-			return (a[aEnd] + b[bEnd])/2;
+			return (double)(a[aEnd] + b[bEnd])/2;
 		}
 		if(aStart == aEnd && bEnd - bStart == 1) {
-			return b[bEnd] >= a[aStart] ? b[bStart] : b[bEnd];
+			int sum = a[aStart] + b[bStart] + b[bEnd];
+			return sum - Math.max(a[aStart], b[bEnd]) - Math.min(a[aStart], b[bStart]);
 		}
 		if(aEnd - aStart == 1 && bStart == bEnd) {
-			return a[aEnd] >= b[bStart] ? a[aStart] : a[aEnd];
+			int sum = a[aStart] + a[aEnd]+ b[bStart];
+			return sum - Math.max(a[aEnd], b[bStart]) - Math.min(a[aStart], b[bStart]);
 		}
 		
 		double aMedian = this.media(a, aStart, aEnd);
@@ -60,7 +58,7 @@ public class MedianOfTwoSortedArrays {
 		
 		if(aMedian == bMedian) {
 			return aMedian;
-		} else if (aMedian > bMedian) { //XXX Note the following equath
+		} else if (aMedian > bMedian) { //XXX Note the following eqth is critical
 			int newAEnd = aStart + (aEnd-aStart)/2;
 			int newBStart = bEnd - (bEnd - bStart)/2;
 			return this.findMedianSortedArrays(a, aStart, newAEnd, b, newBStart, bEnd);
@@ -73,6 +71,9 @@ public class MedianOfTwoSortedArrays {
 	
 	private double media(int a[], int start, int end) {
 		int size = end - start + 1;
-		return size%2 == 1 ? a[start + size/2] : (a[start + size/2] + a[start + size/2 - 1])/2;
+//		System.out.println("size: " + size);
+//		System.out.println(start);
+//		System.out.println(end);
+		return size%2 == 1 ? a[start + size/2] : ((double)(a[start + size/2] + a[start + size/2 - 1]))/2;
 	}
 }
