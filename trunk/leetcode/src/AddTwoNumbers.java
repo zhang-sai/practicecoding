dc0bc2cdf8bf0934d7cb4405e5b9b27dd33580f7
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-04-01 19:38:01 +0000

    add two numbers

--- trunk/leetcode/src/AddTwoNumbers.java
+++ trunk/leetcode/src/AddTwoNumbers.java
@@ -9,6 +9,38 @@ public class AddTwoNumbers {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         // Start typing your Java solution below
         // DO NOT write main() function
+		if(l1 == null || l2 == null) {
+			return null;
+		}
+        ListNode retNode = null;
+        ListNode prevNode = null; 
         
+        int carrier = 0;
+        while(l1 != null || l2 != null) {
+        	int v1 = l1 != null ? l1.val : 0;
+        	int v2 = l2 != null ? l2.val : 0;
+        	int value = v1 + v2 + carrier;
+        	int rValue = value >= 10 ? value % 10 : value; //XXX note this
+        	carrier = value >= 10 ? value / 10 : 0;
+        	
+        	ListNode newNode = new ListNode(rValue);
+        	if(retNode == null) {
+        		retNode = newNode;
+        		prevNode = newNode;
+        	} else {
+        		prevNode.next = newNode;
+        		prevNode = prevNode.next; //XXX do not forget this
+        	}
+        	//reset l1 and l2
+        	l1 = l1 == null ? null : l1.next;
+        	l2 = l2 == null ? null : l2.next;
+        }
+        
+        //XXX donot forget this
+        if(carrier > 0) {
+        	prevNode.next = new ListNode(carrier);
+        }
+        
+        return retNode;
     }
 }
