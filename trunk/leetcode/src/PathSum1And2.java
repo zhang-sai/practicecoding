361a57de90abee7c939d7fc3f719ecc33d2c9a4b
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-04-02 21:02:10 +0000

    the first path sum problem

--- trunk/leetcode/src/PathSum1And2.java
+++ trunk/leetcode/src/PathSum1And2.java
@@ -20,7 +20,21 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 	public boolean hasPathSum(TreeNode root, int sum) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        
+        if(root == null) {
+        	return false;
+        }
+        if(root.left == null && root.right == null && root.val == sum) {
+        	return true;
+        }
+        if(root.left != null) {
+        	if(this.hasPathSum(root.left, sum - root.val)) {
+        		return true;
+        	}
+        }
+        if(root.right != null) {
+        	return this.hasPathSum(root.right, sum - root.val);
+        }
+        return false;
     }
 	
 	/**
