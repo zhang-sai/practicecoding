0fceb96f07e77c0e999a7216ff8712b95ef45a58
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-04-02 17:21:13 +0000

    plus one

--- trunk/leetcode/src/PlusOne.java
+++ trunk/leetcode/src/PlusOne.java
@@ -6,6 +6,34 @@ public class PlusOne {
 	public int[] plusOne(int[] digits) {
         // Start typing your Java solution below
         // DO NOT write main() function
+		if(digits == null || digits.length == 0) {
+			return digits;
+		}
+        int carrier = 0;
+        for(int i = digits.length - 1; i >= 0; i--) {
+        	int v = digits[i];
+        	if(i == digits.length - 1) {
+        		v = digits[i] + 1;
+        	} else {
+        		v = digits[i] + carrier;
+        	}
+        	digits[i] = v % 10;
+            carrier = v/10;
+        	//a short cut
+        	if(carrier == 0) {
+        		break;
+        	}
+        }
         
+        if(carrier > 0) {
+        	int[] newDigits = new int[digits.length + 1];
+        	for(int i = 1; i < digits.length + 1; i++) {
+        		newDigits[i] = digits[i-1];
+        	}
+        	newDigits[0] = carrier;
+        	return newDigits;
+        } else {
+        	return digits;
+        }
     }
 }
