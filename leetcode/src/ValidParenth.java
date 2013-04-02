71a55e2560d10528bf52feff276b9a4e89739add
racezhang <racezhang@gmail.com>
2013-04-02 17:44:19 +0000

    valid parenthesis

--- leetcode/src/ValidParenth.java
+++ leetcode/src/ValidParenth.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 
 /**
  * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
@@ -8,6 +11,39 @@ public class ValidParenth {
 	public boolean isValid(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        
+        if(s == null || s.isEmpty()) {
+        	return true;
+        }
+        List<Character> list = new LinkedList<Character>();
+        for(char c : s.toCharArray()) {
+        	if(list.isEmpty()) {
+        		list.add(c);
+        	} else {
+        		if(c == '(' || c == '[' || c == '{') {
+        			list.add(c);
+        		} else {
+        			if(c == ')') {
+        				if(list.get(list.size() - 1) == '(') {
+        					list.remove(list.size() - 1);
+        				} else {
+        					return false;
+        				}
+        			} else if (c == ']') {
+        				if(list.get(list.size() - 1) == '[') {
+        					list.remove(list.size() - 1);
+        				} else {
+        					return false;
+        				}
+        			} else if (c == '}') {
+        				if(list.get(list.size() - 1) == '{') {
+        					list.remove(list.size() - 1);
+        				} else {
+        					return false;
+        				}
+        			}
+        		}
+        	}
+        }
+        return list.isEmpty();
     }
 }
