d94707b6e2884f5d59307563c0e5a209c38e89b6
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-04-06 23:39:21 +0000

    first part of the spiral matrix

--- trunk/leetcode/src/SpiralMatrix.java
+++ trunk/leetcode/src/SpiralMatrix.java
@@ -14,10 +14,47 @@ Given the following matrix:
 You should return [1,2,3,6,9,8,7,4,5].
  * */
 public class SpiralMatrix {
+	//here is a recusive way:
+	//http://leetcode.com/2010/05/printing-matrix-in-spiral-order.html
 	public ArrayList<Integer> spiralOrder(int[][] matrix) {
         // Start typing your Java solution below
         // DO NOT write main() function
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if(matrix.length == 0) {
+			return list;
+		}
+		int row = matrix.length;
+		int column = matrix[0].length;
+        boolean[][] flag = new boolean[row][column]; //XXX need some extra space here
+        int index = 1;
+        int x = 0;
+        int y = 0;
+        flag[x][y] = true;
+        list.add(matrix[x][y]);
+        while(index ++ < row*column) {
+        	while(y + 1 < column && !flag[x][y+1]) {
+        		y++;
+        		list.add(matrix[x][y]);
+        		flag[x][y] = true;
+        	}
+        	while(x + 1 < row && !flag[x+1][y]) {
+        		x++;
+        		list.add(matrix[x][y]);
+        		flag[x][y] = true;
+        	}
+        	while(y - 1 >=0 && !flag[x][y-1]) {
+        		y--;
+        		list.add(matrix[x][y]);
+        		flag[x][y] = true;
+        	}
+        	while(x - 1 >=0 && !flag[x-1][y]) {
+        		x--;
+        		list.add(matrix[x][y]);
+        		flag[x][y] = true;
+        	}
+        }
         
+        return list;
     }
 	
 	/**
@@ -36,6 +73,36 @@ You should return the following matrix:
 	public int[][] generateMatrix(int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        int[][] matrix = new int[n][n];
+        if(n == 0) {
+        	return matrix;
+        }
+        //start
+        int x = 0;
+        int y = 0;
+        matrix[x][y] = 1;
+        int i = 2;
+        while(i <= n*n) {
+        	if(y + 1 < n && matrix[x][y + 1] == 0) {
+        		y ++;
+        		matrix[x][y] = i;
+        	} else if (x + 1 < n && matrix[x+1][y] == 0) {
+        		x++;
+        		matrix[x][y] = i;
+        	} else if (y - 1 >= 0 && matrix[x][y-1] == 0) {
+        		y--;
+        		matrix[x][y] = i;
+        	} else if (x - 1 >= 0 && matrix[x-1][y] == 0) {
+        		x--;
+        		matrix[x][y] = i;
+        	} else {
+        		throw new Error();
+        	}
+        	
+        	i++;
+        }
         
+        
+        return matrix;
     }
 }
