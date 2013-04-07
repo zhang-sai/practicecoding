55ee60495e8e7661dc2789f27fbabff1b1146f68
racezhang <racezhang@gmail.com>
2013-04-07 08:06:05 +0000

    rotate image

--- leetcode/src/RotateImage.java
+++ leetcode/src/RotateImage.java
@@ -6,8 +6,26 @@ Rotate the image by 90 degrees (clockwise).
 Follow up:
 Could you do this in-place?
  * */
-XX
+
 public class RotateImage {
+	
+	public static void main(String[] args) {
+//		int[][] matrix = new int[][]{new int[]{1, 2, 3, 4}, 
+//				new int[]{5, 6, 7, 8}, 
+//				new int[]{9, 10, 11, 12}, 
+//				new int[]{13, 14, 15, 16}
+//		};
+//		int[][] matrix = new int[][]{new int[]{1, 2, 3}, 
+//				new int[]{4, 5, 6}, 
+//				new int[]{7, 8, 9}
+//		};
+		int[][] matrix = new int[][]{new int[]{1, 2}, 
+				new int[]{3, 4}
+		};
+		RotateImage ri = new RotateImage();
+		ri.rotate(matrix);
+	}
+	
 	//in a spiral way
 	public void rotate(int[][] matrix) {
 		if(matrix.length == 0) {
@@ -23,17 +41,32 @@ public class RotateImage {
 		//basic idea:
 		// original index: i, j ==> (j , n-1-i)
 		
-		for(int x = 0; x < (n / 2) + n%2 /**ceiling*/; x++) {
-			for(int y = x; y < n - x /*not 2*row */; y++) {
+		for(int x = 0; x < (n / 2); x++) {
+			for(int y = x; y < n-x - 1 /*not 2*row */; y++) { //XXX note it is y < n -x - 1
+				//pictorially denoted as:
+				// 1, 2, 3
+				// 4, 5, 6
+				// 7, 8, 9
+				// is divided into four parts: 1,2 == 3, 6 == 9, 8 == 7, 4
+				
+				System.out.println(x + ", " + y);
 				//do rotation here
 				int nextX1 = y;
 				int nextY1 =  n - 1 - x;
 				
+				System.out.println(nextX1 + ", " + nextY1);
+				
 				int nextX2 = nextY1;
 				int nextY2 = n - 1 - nextX1;
 				
+				System.out.println(nextX2 + ", " + nextY2);
+				
 				int nextX3 = nextY2;
 				int nextY3 = n - 1 - nextX2;
+				
+				System.out.println(nextX3 + ", " + nextY3);
+				
+				
 				
 				int tmp = matrix[nextX3][nextY3];
 				matrix[nextX3][nextY3] = matrix[nextX2][nextY2];
@@ -41,6 +74,8 @@ public class RotateImage {
 				matrix[nextX1][nextY1] = matrix[x][y];
 				matrix[x][y] = tmp;
 				
+				
+				System.out.println("---");
 			}
 		}
     }
