bd937d2f2280a141dcc8488b26a0506a62cc60e5
racezhang <racezhang@gmail.com>
2013-04-12 21:32:43 +0000

    remove duplication in a sorted list

--- leetcode/src/RemoveDuplicatesSortedList.java
+++ leetcode/src/RemoveDuplicatesSortedList.java
@@ -38,7 +38,31 @@ For example,
 Given 1->2->3->3->4->4->5, return 1->2->5.
 Given 1->1->1->2->3, return 2->3.
 	 * */
+	//a sample answer:
+	//http://fenghaolw.blogspot.com/2013/03/remove-duplicates-from-sorted-list-ii.html
 	public ListNode deleteDuplicates_all_code(ListNode head) {
-        xxx
+		//XXX use a tmp node is much convenient
+        ListNode tmpNode = new ListNode(-1);
+        tmpNode.next = head;
+        
+        ListNode prev = tmpNode;
+        ListNode curr = head;
+        
+        while(curr != null) {
+        	boolean dup = false;
+        	while(curr.next != null && curr.next.val == curr.val) {
+        		dup = true;
+        		curr = curr.next;
+        	}
+        	if(dup) {
+        		prev.next = curr.next;
+        	} else {
+        		//no duplicate
+        		prev = curr;  //XXX no dup means only 1 step
+        	}
+        	curr = curr.next;
+        }
+        
+        return tmpNode.next;
     }
 }
