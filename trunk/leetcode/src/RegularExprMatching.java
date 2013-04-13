778fda39299a9254e07c66076e8b61c7c13623df
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-04-13 05:50:47 +0000

    regular exp matching

--- trunk/leetcode/src/RegularExprMatching.java
+++ trunk/leetcode/src/RegularExprMatching.java
@@ -10,73 +10,65 @@ The function prototype should be:
 bool isMatch(const char *s, const char *p)
 
  * */
+//http://leetcode.com/2011/09/regular-expression-matching.html
 public class RegularExprMatching {
-	public boolean isMatch(String s, String p) {
-		XXXX
-    }
 	
-	public boolean isMatch_Wrong_Question(String s, String p) {
+	public static void main(String[] args) {
+		RegularExprMatching rem = new RegularExprMatching();
+//		System.out.println(rem.isMatch("ab", ".*c"));
+//		System.out.println(rem.isMatch("aab", "c*a*b"));
+//		System.out.println(rem.isMatch("bbbba", ".*a*a"));
+//		System.out.println(rem.isMatch("aaba", "ab*a*c*a"));
+		System.out.println(rem.isMatch("aa", "a*"));
+	}
+	//Note p.charAt(0) != *
+	public boolean isMatch(String s, String p) {
+		System.out.println("matching: " + s + ",  " + p);
+		if(s == null || p == null) {
+			return false;
+		}
+		//no char in the pattern
 		if(p.isEmpty()) {
 			return s.isEmpty();
 		}
-		int numOfStar = 0;
-		for(char c : p.toCharArray()) {
-			if(c == '*') numOfStar ++;
+		//NO need to consider the case that s is empty
+		if(p.length() == 1) { //cannot be just a *, two possibilities
+			
+			boolean r = s.length() == 1 && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.');
+			System.out.println("  match: " + s + ", " + p + " -> " + r);
+			return r;
 		}
-		if(p.length() - numOfStar > s.length()) {
-			return false;
-		}
-		if(s.isEmpty()) {
-			return p.length() - numOfStar == 0;
-		}
-		
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        boolean[][] match = new boolean[p.length()][s.length()];
-        
-        //compute the first row
-        char pChar = p.charAt(0);
-        for(int i = 0; i < s.length(); i++) {
-        	if(pChar == '*') {
-        		match[0][i] = true;
-        	} else if(pChar == '.') {
-        		match[0][i] = (i == 0) ? true : false;
-        	} else {
-        		match[0][i] = (i == 0 && s.charAt(0) == pChar) ? true : false;
-        	}
-        }
-        
-        //compute the first column
-        char sChar = s.charAt(0);
-		if (pChar == '*') {
-			for (int i = 1; i < p.length(); i++) {
-				if (p.charAt(i) == '*') {
-					match[i][0] = match[i - 1][0];
-				} else if (p.charAt(i) == '.') {
-					match[i][0] = (p.charAt(i - 1) == '*' && match[i - 1][0]) ? true : false;
-					break; // XXX MUST break;
+		//need to take care of *
+		if(p.charAt(1) != '*') {
+			if(s.isEmpty()) {
+				return false;
+			} else {
+				if(s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') {
+					return this.isMatch(s.substring(1), p.substring(1));
 				} else {
-					match[i][0] = (p.charAt(i - 1) == '*' && match[i - 1][0] && sChar == p.charAt(i)) ? true : false;
-					break; // XXX must break;
+					return false;
 				}
 			}
-        }
-        
-        //compute the rest cells
-        for(int j = 1; j < s.length(); j++) {
-        	char sc = s.charAt(j);
-        	for(int i = 1; i < p.length(); i++) {
-        		char pc = p.charAt(i);
-        		if(pc == '*') {
-        			match[i][j] = match[i-1][j-1] || match[i-1][j];
-        		} else if (pc == '.') {
-        			match[i][j] = match[i-1][j-1]; //match any char
-        		} else {
-        			match[i][j] = match[i-1][j-1] && sc == pc; 
-        		}
-        	}
-        }
-        
-        return match[p.length() - 1][s.length() - 1];
+		} else { //the next is *
+			if(s.isEmpty()) {
+				return this.isMatch(s, p.substring(2));
+			} else {
+				//it can match 0 or many 
+				for(int i = 0; i < s.length(); i++) {
+					if(s.charAt(i) == p.charAt(0) || p.charAt(0) == '.') {
+						System.out.println(" recusive matching: " + s.substring(i + 1) + "  with " + p.substring(2));
+						if(this.isMatch(s.substring(i + 1), p.substring(2))) {
+							return true;
+						}
+					} else {
+						break; //XXX do not forget break it out
+					}
+				}
+			}
+		}
+		
+		return this.isMatch(s, p.substring(2)); 
+		//XXX return match nothing of s
     }
 }
+
