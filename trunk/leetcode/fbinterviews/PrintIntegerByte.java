bccdacc7bccdcfb5cf4d1d2c89e43c4498e793b1
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-04-17 20:51:28 +0000

    more questions

---
+++ trunk/leetcode/fbinterviews/PrintIntegerByte.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * byte[] printbyte(int a){
+    
+    byte result [] = new byte[4];
+    
+    result[0] = a>>>24&0xFF;
+    result[1] = a>>>16&oxFF;
+    result[2] = a>>>8&oxFF;
+    result[3] = a&oxFF;
+    return result;
+    }
+ * */
+public class PrintIntegerByte {
+
+}
---
+++ trunk/leetcode/fbinterviews/SortLinkedList.java
@@ -0,0 +1,8 @@
+/**
+ *  Sort a linked list in O(nlogn) time with o(1) space complexity. Recursion
+doesn't work here.
+http://www.mitbbs.com/article_t/JobHunting/32394795.html
+ * */
+public class SortLinkedList {
+
+}
