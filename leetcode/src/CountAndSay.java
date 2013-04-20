8ba5e62e58f06f41d56399faf47870c5976c6791
racezhang <racezhang@gmail.com>
2013-04-20 19:32:14 +0000

    count and say

--- leetcode/src/CountAndSay.java
+++ leetcode/src/CountAndSay.java
@@ -10,10 +10,43 @@ Given an integer n, generate the nth sequence.
 
 Note: The sequence of integers will be represented as a string.
  * */
+
 public class CountAndSay {
 	public String countAndSay(int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        int round = 1;
+        String value = "1";
+        while(round++ < n) {
+        	StringBuilder sb = new StringBuilder();
+        	char[] cs = value.toCharArray();
+        	char currChar = cs[0];
+        	int currCount = 1;
+        	for(int i = 1; i < cs.length; i++) {
+        			if(currChar != cs[i]) {
+        				sb.append(currCount);
+//        				System.out.println("now sb: " + sb.toString());
+        				sb.append(String.valueOf(currChar));
+//        				System.out.println("now sb: " + sb.toString());
+        				currChar = cs[i];
+        				currCount = 1;
+        			} else {
+        				currCount++;
+        			}
+        	}
+        	//XXX donot forget this step
+        	sb.append(currCount);
+			sb.append(String.valueOf(currChar));
+        	value = sb.toString();
+        }
         
+        return value;
     }
+	
+	public static void main(String[] args) {
+		CountAndSay cas = new CountAndSay();
+		for(int i = 0; i < 10; i++) {
+			System.out.println(cas.countAndSay(i));
+		}
+	}
 }
