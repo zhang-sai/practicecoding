4ed29702e567639a6d6daef9400a4466c1f3e61d
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-04-27 03:52:09 +0000

    combination sum

--- trunk/leetcode/src/CombinationsSum1And2.java
+++ trunk/leetcode/src/CombinationsSum1And2.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class CombinationsSum1And2 {
@@ -18,14 +21,75 @@ A solution set is:
 [7] 
 [2, 2, 3] 
 	 * */
-	XXX
+	
+	//pass both tests
+	//other ppl's answer: https://gist.github.com/eclipse9614/4983552
+	//this version is not good, it uses set, the second implementation is better!
 	 public ArrayList<ArrayList<Integer>> combinationSum(int[] num, int target) {
 	        // Start typing your Java solution below
 	        // DO NOT write main() function
 	        Arrays.sort(num);
 	        
-	        return null;
+	        if(target < num[0]) {
+	        	return new ArrayList<ArrayList<Integer>>();
+	        }
+	        
+	        Set<ArrayList<Integer>> all = new HashSet<ArrayList<Integer>>();
+	        
+	        for(int n : num) {
+//	        	System.out.println("n: " + n + ", target: " + target);
+	        	if (n == target) {
+	        		ArrayList<Integer> l = new ArrayList<Integer>();
+	        		l.add(n);
+	        		all.add(l);
+	        	} else if(n < target) {
+	        	    ArrayList<ArrayList<Integer>> remainList = combinationSum(num, target - n);
+//	        	    if(remainList.isEmpty()) {
+//	        	    	continue;
+//	        	    }
+	        	    for(ArrayList<Integer> l : remainList) {
+	        	    	ArrayList<Integer> exl = new ArrayList<Integer>(l);
+	        	    	exl.add(n);
+	        	    	Collections.sort(exl);
+	        	    	all.add(exl);
+	        	    }
+	        	} else {
+//	        		System.out.println("set: " + all);
+	        		return new ArrayList<ArrayList<Integer>>(all);
+	        	}
+	        }
+//	        System.out.println("set: " + all);
+	        return new ArrayList<ArrayList<Integer>>(all);
 	    }
+	 
+	 //====================another version of combination sum of duplications=======
+	 //this version seems better
+	 public ArrayList<ArrayList<Integer>> combinationSum_(int[] num, int target) {
+		 Arrays.sort(num);
+		 ArrayList<Integer> currResult = new ArrayList<Integer>();
+		 ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
+		 this.solve(num, target, results, 0, currResult);
+		 return results;
+	 }
+	 
+	 public void solve(int[] num, int target, ArrayList<ArrayList<Integer>> results, int curr, ArrayList<Integer> currResult) {
+		 if(target < 0) {
+			 return;
+		 } else if (target == 0) {
+			 ArrayList<Integer> result = new ArrayList<Integer>(currResult);
+			 results.add(result);
+			 return;
+		 }
+		 
+		 for(int i = curr; i < num.length; i++) {
+			 currResult.add(num[i]);
+			 this.solve(num, target - num[i], results, i, /**XXX THe most important part, use i*/ currResult); //you can never track back
+			 //such as 2, 3, 5, 7 ==> 7
+			 //you would only produce: 2, 2, 3 once
+			 currResult.remove(currResult.size() - 1);
+		 }
+	 }
+	 //=============================================================================
 	 
 	 /**
 	  * Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
@@ -99,7 +163,8 @@ A solution set is:
 	 
 	 public static void main(String[] args) {
 		 CombinationsSum1And2 c = new CombinationsSum1And2();
-		 System.out.println(c.combinationSum2(new int[]{1}, 1));
+//		 System.out.println(c.combinationSum2(new int[]{1}, 1));
+		 System.out.println(c.combinationSum(new int[]{2, 3, 6, 7}, 7));
 	 }
 	
 }
