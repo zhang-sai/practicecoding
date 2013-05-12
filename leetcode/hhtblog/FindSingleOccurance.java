f696d7d009b46895ef08e00cf6b81a0db8fd3db2
racezhang <racezhang@gmail.com>
2013-05-12 23:15:37 +0000

    adds new question

---
+++ leetcode/hhtblog/FindSingleOccurance.java
@@ -0,0 +1,13 @@
+//http://zhedahht.blog.163.com/blog/static/2541117420071128950682/
+//http://zhedahht.blog.163.com/blog/static/25411174201283084246412/
+public class FindSingleOccurance {
+
+	public int[] getTwoSingleOccurance(int[] nums) {
+		
+	}
+	
+	public int[] getThreeSingleOccurance(int[] nums) {
+		
+	}
+	
+}
