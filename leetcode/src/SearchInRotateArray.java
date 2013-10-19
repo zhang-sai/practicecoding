197daf1d76ff37574451f4c45b739f166475de3b
racezhang <racezhang@gmail.com>
2013-10-19 05:44:07 +0000

    Search in a sorted array

--- leetcode/src/SearchInRotateArray.java
+++ leetcode/src/SearchInRotateArray.java
@@ -14,44 +14,33 @@ You may assume no duplicate exists in the array.
 public class SearchInRotateArray {
 
 	//suppose the array is sorted in increasing order
-	public int search(int[] a, int target) {
-        return this.findTarget(a, target, 0, a.length - 1);
+	public int search(int[] A, int target) {
+        int l = 0;
+        int r = A.length -1;  
+        while(l<=r) {   
+            int m = (l+r)/2;   
+            if(A[m] == target) {
+                return m;   
+            }
+            //the left part is sorted
+            if(A[m]>= A[l]) {   
+                if(A[l]<=target && target<= A[m]) {
+                      r=m-1;   
+                } else  {
+                      l = m+1;       
+                }
+            } else {
+                //the right part is sorted
+                if(A[m] <= target && target <= A[r]) {
+                      l = m+1;    
+                } else {
+                      r = m-1;   
+                }
+            }   
+        }   
+        //end of search, find nothing
+        return -1;   
     }
-	
-	public int findTarget(int[] a, int target, int startIndex, int endIndex) {
-		if(a.length == 0) {
-        	return -1;
-        }
-		if(startIndex > endIndex) {
-			return -1;
-		}
-		int midIndex = (startIndex + endIndex)/2;
-		if(a[midIndex] == target) {
-			return midIndex;
-		} else {
-    		//decide which part should search
-    		//here is some possibilities
-    		if(a[startIndex] < a[endIndex]) {
-    			//the array is sorted, the normal binary search
-    			if(a[midIndex] > target) {
-    				return this.findTarget(a, target, startIndex, midIndex - 1);
-    			} else {
-    				return this.findTarget(a, target, midIndex + 1, endIndex);
-    			}
-    		} else {
-    			//XXX in this case there are always 2 possibility
-    			//3, 5, 1 ==> find 3
-    			//3, 5, 1 ==> find 1
-    			int index1 = this.findTarget(a, target, startIndex, midIndex - 1);
-    			if(index1 == -1) {
-				    int index2 = this.findTarget(a, target, midIndex + 1, endIndex);
-				    return index2;
-    			} else {
-    				return index1;
-    			}
-    		}
-    	}
-	}
 	
 	public static void main(String[] args) {
 		SearchInRotateArray s = new SearchInRotateArray();
@@ -67,31 +56,38 @@ Would this affect the run-time complexity? How and why?
 Write a function to determine if a given target is in the array.
 	 * 
 	 * */
-	xx
+//	xx
 	//adapt from: http://gongxuns.blogspot.com/2012/12/leetcode-search-in-rotated-sorted-array_9.html
-	public boolean search_dup(int[] a, int target) {
+	public boolean search_dup(int[] A, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int start = 0;
-        int end = a.length - 1;
-        while(start <= end) {
-        	int mid = (start + end)/2;
-        	if(a[mid] == target) {
-        		return true;
-        	} else if (a[mid] > target) {
-        		
-        	} else { //a[mid] < target
-        		//three possibilities:
-        		//1. the most normal way
-        		if(a[mid] > a[end] || a[end] >= target) { //1.  [3,4,5,6,1,2] target = 6
-        			start = mid + 1;                      //2.  [2,3,4,5,6] target = 4
-        		} else if (a[end] > a[mid] && a[end] < target) {   // [1, 3, 5, 7, 2, 4] target = 7
-        			
-        		} else {
-        			
-        		}
-        	}
-        }
-        return false;
+		int l = 0;
+        int r = A.length -1;  
+        while(l<=r) {   
+            int m = (l+r)/2;   
+            if(A[m] == target) {
+                return true;   
+            }
+            //the left part is sorted
+            if(A[m]> A[l]) {   
+                if(A[l]<=target && target<= A[m]) {
+                      r=m-1;   
+                } else  {
+                      l = m+1;       
+                }
+            } else if(A[m] < A[l]) {
+                //the right part is sorted
+                if(A[m] <= target && target <= A[r]) {
+                      l = m+1;    
+                } else {
+                      r = m-1;   
+                }
+            } else {
+                l++; //THE ONLY DIFF PART, IN THIS CASE WE HAVE NO CHOICE
+                //like  1, 3, 1, 1, 1, 1
+            }
+        }   
+        //end of search, find nothing
+        return false;  
     }
 }
