9e4f674599a70b6419415203385d78c6816e8526
racezhang <racezhang@gmail.com>
2013-10-24 16:52:17 +0000

    re-rog

---
+++ leetcode/fbinterviews/FindAllCommonElements.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/***
+ * N sorted array, find elements appears in all of them
+ * e.g.,
+ *  [1, 2, 3, 4]   [2, 4, 6, 8]   [0, 2, 3, 4, 10]
+ *  
+ *  output: [2, 4]
+ * */
+public class FindAllCommonElements {
+
+	public List<Integer> findAllCommons(List<List<Integer>> lists) {
+		List<Integer> retList = new ArrayList<Integer>();
+		
+		
+		
+		return retList;
+	}
+	
+}
