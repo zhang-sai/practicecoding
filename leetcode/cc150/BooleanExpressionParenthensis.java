42108ef66570d1315a2e9f4eb97330ce995fd922
racezhang <racezhang@gmail.com>
2013-11-01 16:50:56 +0000

    more questions

---
+++ leetcode/cc150/BooleanExpressionParenthensis.java
@@ -0,0 +1,12 @@
+/**
+ * Given a boolen expression, and a result,
+ * find ways to count the number of ways of
+ * parenthesizing it.
+ * 
+ * e.g., 1^0|0|1  result = 0
+ * 
+ * two ways: 1^((0|0)|1)  and 1^(0|(0|1)) 
+ * */
+public class BooleanExpressionParenthensis {
+
+}
---
+++ leetcode/cc150/SearchMagicNumber.java
@@ -0,0 +1,6 @@
+//a[i] = i
+//in a sorted array, which may contain duplications
+//9.3
+public class SearchMagicNumber {
+
+}
