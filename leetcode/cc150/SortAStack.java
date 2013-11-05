7c7ddfcaf76108d4168ee85a50926993ef76b607
racezhang <racezhang@gmail.com>
2013-11-05 03:40:37 +0000

    sort a stack

--- leetcode/cc150/SortAStack.java
+++ leetcode/cc150/SortAStack.java
@@ -1,7 +1,48 @@
+import java.util.Stack;
+
 
 /**
  * Sort a stack with another stack
  * */
 public class SortAStack {
 
+	//use another stack
+	
+	public static void sortStack(Stack<Integer> s) {
+		Stack<Integer> tmp = new Stack<Integer>();
+		tmp.push(s.pop());
+		
+		//if the original stack has some element
+		while(!s.isEmpty()) {
+			Integer v = s.pop();
+			while( !tmp.isEmpty() && v > tmp.peek()) {
+				s.push(tmp.pop());
+			}
+			tmp.push(v);
+		}
+		
+		//go back to s
+		while(!tmp.isEmpty()) {
+			s.push(tmp.pop());
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Stack<Integer> s = new Stack<Integer>();
+		s.push(1);
+		s.push(3);
+		s.push(2);
+		s.push(10);
+		s.push(7);
+		s.push(6);
+		s.push(11);
+		s.push(8);
+		s.push(9);
+		sortStack(s);
+		while(!s.isEmpty()) {
+			System.out.println(s.pop());
+		}
+	}
+	
 }
