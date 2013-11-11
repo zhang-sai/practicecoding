b0d2196e7dfc7c4f5da701e80e133725c6b3ae13
racezhang <racezhang@gmail.com>
2013-11-11 22:53:54 +0000

    add without arithemtic

--- leetcode/hhtblog/AddWithoutArithmetic.java
+++ leetcode/hhtblog/AddWithoutArithmetic.java
@@ -1,4 +1,25 @@
 //http://zhedahht.blog.163.com/blog/static/254111742011125100605/
 public class AddWithoutArithmetic {
 
+	static int addWithoutArithmetic(int num1, int num2) {
+		if(num1 == 0) {
+            return num2;
+		}
+		if(num2 == 0) {
+			return num1;
+		}
+
+		//the sum
+        int sum = num1 ^ num2;
+        
+        //the carrier
+        int carry = (num1 & num2) << 1; //only when both bits are 1, so goes to the next bit
+
+        return addWithoutArithmetic(sum, carry);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(addWithoutArithmetic(1, 3));
+	}
+	
 }
