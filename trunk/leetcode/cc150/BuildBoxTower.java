bf1ba53006dd519c350b7e51cd40be0b2236378b
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-11 20:11:53 +0000

    LIS

--- trunk/leetcode/cc150/BuildBoxTower.java
+++ trunk/leetcode/cc150/BuildBoxTower.java
@@ -1,6 +1,27 @@
-x
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
 //a stack of box, each has w, h, d
 //can only be put on top of one another if 3 dims are strictly larger
 public class BuildBoxTower {
 
+	//use the longest substring
+	
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(1);
+		list.add(5);
+		list.add(4);
+		list.add(2);
+		
+		System.out.println(list);
+		
+		Iterator<Integer> it = list.iterator();
+		it.next();
+		it.next();
+		it.remove();
+		System.out.println(list);
+	}
 }
+
