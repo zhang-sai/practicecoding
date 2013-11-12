75510ad3723d35b3a54b7d0270a94f5664d4b262
racezhang <racezhang@gmail.com>
2013-11-12 23:12:08 +0000

    rank in stream values

--- leetcode/cc150/RankOfStreamValue.java
+++ leetcode/cc150/RankOfStreamValue.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
 import java.util.PriorityQueue;
-xxxx
+
+import util.Utils;
+
 //11.8
+
+class TNode {
+    int val;
+    TNode left;
+    TNode right;
+    TNode(int x) { val = x; }
+    public String toString() {return val + ""; }
+    
+    int elem = 1; //repetitive number
+    int gnum = 0;
+    
+}
+
 public class RankOfStreamValue {
 
 	/**
@@ -8,23 +24,88 @@ public class RankOfStreamValue {
 	 * element less or equal than it
 	 * */
 	
-	public static void processStream(int[] array) {
+	public static TNode processStream(Integer[] array) {
 		//it build a tree, the number element = numbers <= it
-		TreeNode root = new TreeNode(array[0]);
-		root.elem++;
+		TNode root = new TNode(array[0]);
 		for(int i = 1; i < array.length; i++) {
 			int v = array[i];
-			TreeNode curr = root;
+			TNode curr = root;
 			while(curr != null) {
 				if(curr.val == v) {
-					
+					curr.elem++;
+					break; //break the loop
+				} else {
+					if(curr.val > v) {
+						//go to the left side
+						curr.gnum++;
+						if(curr.left == null) {
+							curr.left = new TNode(v);
+							break; //break the loop
+						} else {
+							curr = curr.left;
+						}
+					} else {
+						//go to the right side
+						//do not increase the gnum
+						if(curr.right == null) {
+							curr.right = new TNode(v);
+							break; //break the loop
+						} else {
+							curr = curr.right;
+						}
+					}
 				}
 			}
 		}
+		
+		return root;
+	}
+	
+	public static int queryRank (TNode root, int v) {
+		int rank = 0;
+		TNode curr = root;
+		while(curr != null) {
+			if(curr.val == v) {
+				rank += curr.gnum;
+				return rank;
+			} else if (curr.val > v) {
+				curr = curr.left;
+			} else {
+				rank += curr.gnum;
+				rank += curr.elem;
+				curr = curr.right;
+			}
+		}
+		return -1;
 	}
 	
 	public static void main(String[] args) {
-		int[] array = new int[]{5, 4, 5, 1, 3, 4, 5, 6, 7, 8, 9, 10, 3, 2, 4, 5, 6, 11};
-		processStream(array);
+		Integer[] array = new Integer[]{5, 4, 5, 1, 3, 4, 5, 6, 7, 8, 9, 10, 3, 2, 4, 5, 6, 11};
+		
+		TNode root = processStream(array);
+		System.out.println("The rank of 5: " + queryRank(root, 5));
+		System.out.println("The rank of 6: " + queryRank(root, 6));
+		System.out.println("The rank of 3: " + queryRank(root, 3));
+		System.out.println("The rank of 4: " + queryRank(root, 4));
+		System.out.println("The rank of 1: " + queryRank(root, 1));
+		System.out.println("The rank of 8: " + queryRank(root, 8));
+		
+		Arrays.sort(array);
+		System.out.println(Utils.dumpArray(array));
+		
+		//print a tree
+		printTree(root, 0);
+	}
+	
+	static void printTree(TNode t, int indent) {
+		if(t == null) {
+			return;
+		}
+		for(int i = 0; i < indent; i++) {
+			System.out.print(" ");
+		}
+		System.out.println(t.val + ", gnum: " + t.gnum + ",  elem: " + t.elem);
+		printTree(t.left, indent + 3);
+		printTree(t.right, indent + 3);
 	}
 }
