3ece181aa4f0c42fdcf47721a1d65b1ab0c7d56a
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-12 23:57:05 +0000

    add punctution

--- trunk/leetcode/cc150/AddingPunctutionToText.java
+++ trunk/leetcode/cc150/AddingPunctutionToText.java
@@ -1,9 +1,13 @@
-x
+
 public class AddingPunctutionToText {
 
 	//17.14
 	public String addPunct(String text) {
 		return text;
+		
+		//a typical dp
+		//T(n) = min(T(i) + str(i, ... n))
+		//complexity: O(n2)
 	}
 	
 }
