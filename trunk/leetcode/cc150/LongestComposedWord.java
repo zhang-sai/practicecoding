eea04ab7c25ec983bdb24cfdc0a70cd63f82eb42
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-13 19:47:56 +0000

    longest composed word

--- trunk/leetcode/cc150/LongestComposedWord.java
+++ trunk/leetcode/cc150/LongestComposedWord.java
@@ -1,10 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 //find the longest composed word of other words
 //["a", "dog", "cat", "pig", "walker"] ==> "dogwalker"
 public class LongestComposedWord {
 
-	public String getLongestWord(String[] words) {
-		return null;
+	static class TrieNode {
+		Character c;
+		Map<Character, TrieNode> map = new HashMap<Character, TrieNode>();
+		public TrieNode(Character  t) {
+			c = t;
+		}
+	}
+	
+	public static String getLongestWord(String[] words) {
+		String currLongest = "";
+		
+		TrieNode root = new TrieNode('#'); //a dummy node
+		for(String w : words) {
+			boolean isPrefix = true;
+			TrieNode curr = root;
+			for(Character c : w.toCharArray()) {
+				if(curr.map.containsKey(c)) {
+					continue;
+				} else {
+					TrieNode newNode = new TrieNode(c);
+					curr.map.put(c, newNode);
+					curr = newNode;
+					isPrefix = false;
+				}
+			}
+			if(isPrefix) {
+				if(w.length() > currLongest.length()) {
+					currLongest = w;
+				}
+			}
+		}
+		
+		return currLongest;
+	}
+	
+	public static void main(String[] args) {
+		String[] words = new String[]{
+				"cat", "cats", "catsdogcats", "catxdogcatsrat",
+				"dog", "dogcatsdog", "hippopotamuses", 
+				"rat", "ratcat", "ratcatdog", "ratcatdogcat"
+		};
+		System.out.println(getLongestWord(words));
 	}
 	
 }
