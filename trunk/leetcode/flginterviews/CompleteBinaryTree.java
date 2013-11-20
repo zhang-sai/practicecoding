498964282afec12088cd67fc94c673124656286f
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-20 22:42:06 +0000

    complete binary tree

---
+++ trunk/leetcode/flginterviews/CompleteBinaryTree.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ * Write a function to validate a complete binary tree. That is, the function 
+will take a binary tree as an input argument and return true if the given 
+binary tree is a complete binary tree.
+
+A binary tree is complete if all levels except possibly the last are 
+completely full, and the last level has all its node to the left side.
+
+For example, the following is a complete binary tree:
+
+
+       a
+
+     /  \
+
+    b     c
+
+   /    
+
+  d    
+
+The followings are not complete binary trees:
+
+
+      a
+
+    /   \
+
+   b     c
+
+        / \
+
+       d  e
+
+
+      a
+
+    /   \
+
+   b     c
+
+  /     / \
+
+f     d    e
+ * */
+public class CompleteBinaryTree {
+	
+	//level by leve traverse, and check the next level
+	//a flag indicate noMoreRight Node
+
+}
