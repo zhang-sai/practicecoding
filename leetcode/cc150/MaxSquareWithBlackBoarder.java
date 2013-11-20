57a1807ea3bf8c710731f91b0959ee75b64d4fa0
racezhang <racezhang@gmail.com>
2013-11-20 22:54:35 +0000

    black board

--- leetcode/cc150/MaxSquareWithBlackBoarder.java
+++ leetcode/cc150/MaxSquareWithBlackBoarder.java
@@ -2,6 +2,12 @@
 //explanation: http://discuss.joelonsoftware.com/default.asp?interview.11.511988.16
 public class MaxSquareWithBlackBoarder {
 
-	xx
+	//a O(n3) algorithm
+	//preprocess to record the number of next black nodes in the left or down directions
+	//then do brute force search
+	//for (length = n to 1) {
+	//    for(int startpointX = 1... )
+	//        for(int startY = 1.... )
+    //}
 	
 }
