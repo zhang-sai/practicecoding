3197424a34a03f9cd4c2392571d09439fbc1c1d5
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-20 22:36:08 +0000

    cc 150

--- trunk/leetcode/cc150/MaxSquareWithBlackBoarder.java
+++ trunk/leetcode/cc150/MaxSquareWithBlackBoarder.java
@@ -1,4 +1,7 @@
 //a square, find the largest square with four corner black
+//explanation: http://discuss.joelonsoftware.com/default.asp?interview.11.511988.16
 public class MaxSquareWithBlackBoarder {
 
+	xx
+	
 }
