96724a1900f6cf6a0db2640b0fd15e768ce00910
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-20 22:35:14 +0000

    update

---
+++ trunk/leetcode/src/basics/PrimeFactors.java
@@ -0,0 +1,38 @@
+package basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * print out prime factors. e.g., 20=2x2x5, 90=2x3x3x5 How to get a list of
+ * prime numbers
+ * */
+public class PrimeFactors {
+	
+	public static List<Integer> primeFactors(int number) {
+		int n = number;
+		List<Integer> factors = new ArrayList<Integer>();
+		for (int i = 2; i <= n/i; i++) { //note it is n/i
+			while (n % i == 0) {
+				factors.add(i);
+				n /= i;
+			}
+		}
+		return factors;
+	}
+	
+	public static void main(String[] args) {
+	    System.out.println("Primefactors of 44");
+	    for (Integer integer : primeFactors(44)) {
+	      System.out.println(integer);
+	    }
+	    System.out.println("Primefactors of 3");
+	    for (Integer integer : primeFactors(3)) {
+	      System.out.println(integer);
+	    }
+	    System.out.println("Primefactors of 32");
+	    for (Integer integer : primeFactors(32)) {
+	      System.out.println(integer);
+	    }
+	  }
+}
