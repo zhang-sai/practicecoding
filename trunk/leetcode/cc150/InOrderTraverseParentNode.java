b1d0c8286f6ed93713738830dc56baa625a0240d
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-20 23:07:37 +0000

    first try

--- trunk/leetcode/cc150/InOrderTraverseParentNode.java
+++ trunk/leetcode/cc150/InOrderTraverseParentNode.java
@@ -1,5 +1,31 @@
 
-x
-public class InOrderTraverseParentNode {
 
+public class InOrderTraverseParentNode {
+xx
+	//some basic ideas
+	public TreeNode getNext(TreeNode node) {
+		
+		if(node.left != null) {
+			TreeNode n = node;
+			while(n.left != null) {
+				n = n.left;
+			}
+			return n;
+		} else if(node.right != null) {
+			return getNext(node.right);
+		} else {
+			//go up until node.parent.left = node
+			TreeNode parent = node.parent;
+			while(parent != null) {
+				if(parent.left == node) {
+					break;
+				}
+				node = parent;
+				parent = node.parent;
+			}
+			return parent;
+		}
+		
+	}
+	
 }
