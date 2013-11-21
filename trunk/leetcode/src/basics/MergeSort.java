40c8c714ec47035eb6b39623f8cd0838201c9558
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-21 04:31:13 +0000

    merge sort

--- trunk/leetcode/src/basics/MergeSort.java
+++ trunk/leetcode/src/basics/MergeSort.java
@@ -1,5 +1,62 @@
 package basics;
-x
+
+import java.util.Random;
+
 public class MergeSort {
 
+	public void mergeSort(int[] a) {
+		if(a.length == 0) {
+			return;
+		}
+		mergeSort(a, 0, a.length - 1);
+	}
+	
+	public void mergeSort(int[] a, int low, int high) {
+		if(low == high) {
+			return;
+		}
+		int mid = (low + high)/2;
+		mergeSort(a, low, mid);
+		mergeSort(a, mid + 1, high);
+		//then perform merging
+		//[low, mid] and [mid + 1, high] have been sorted
+		//copy array
+		int[] array = new int[a.length];
+		int index = array.length - 1;
+		int i = mid;
+		int j = high;
+		while(i >= low && j > mid) {
+			if(a[i] > a[j]) {
+				array[index--] = a[i--];
+			} else {
+				array[index--] = a[j--];
+			}
+		}
+		while(i>=low) {
+			array[index--] = a[i--];
+		}
+		
+		while(j>mid) {
+			array[index--] = a[j--];
+		}
+		
+		for(index = 0; index < array.length; index++) {
+			a[index] = array[index];
+		}
+	}
+	
+	public static void main(String[] args) {
+		Random r = new Random(System.currentTimeMillis());
+		for (int num = 0; num < 200; num++) {
+			int[] a = new int[100 + r.nextInt(200)];
+			for (int i = 0; i < a.length; i++) {
+				a[i] = r.nextInt(500);
+			}
+			MergeSort sort = new MergeSort();
+			sort.mergeSort(a);
+			if (!QuickSort.isSortedAsc(a)) {
+				System.out.println("failed");
+			}
+		}
+	}
 }
