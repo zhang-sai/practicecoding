a3db4b77e1540b73885160b481ddad601d2bb8ac
racezhang <racezhang@gmail.com>
2013-11-23 00:50:19 +0000

    binary tree kth smallest

---
+++ leetcode/flginterviews/BinaryTreeKthSmallest.java
@@ -0,0 +1,9 @@
+/**
+ * Given a binary search tree, write an algorithm to find the kth smallest 
+element 
+ * */
+public class BinaryTreeKthSmallest {
+
+	//perform in order traverse iteratively, and stop when there is a match
+	
+}
