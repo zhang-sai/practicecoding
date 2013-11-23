141b47a449efc39b8045870d763a817488a371c7
racezhang <racezhang@gmail.com>
2013-11-23 01:07:53 +0000

    non adjacent sum

--- leetcode/flginterviews/NonAdjacentSumInAnArray.java
+++ leetcode/flginterviews/NonAdjacentSumInAnArray.java
@@ -2,7 +2,15 @@
  * given an array, find out the max sum of a set that each elements are non-
 : ajacent
  * */
-x
+
 public class NonAdjacentSumInAnArray {
 
+	//a typical db
+	//maxsum[i] ==> the largest sum of a set so far
+	//maxsum[0] = a[0]
+	//maxsum[i] =
+	//       1. take i, value[i] + maxsum[i-2]
+	//       2. not take i, take maxsum[i-1]
+	//a O(n)
+	
 }
