6653ab33097a40813c3459b23bb935ac1f5c5a62
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-24 21:24:17 +0000

    elevation

---
+++ trunk/leetcode/beautyofprog/Evaluation.java
@@ -0,0 +1,45 @@
+
+/**
+ * Stop the evaluation on a certain flow, to minimize the steps every one need to take
+ * */
+public class Evaluation {
+
+	
+	//persons start with 1
+	public int getSteps(int[] persons) { //the number of person on every flow {
+		int minFloor = 0;
+		
+		//stop at the first floor
+		int n1 = 0;  //living below the current floor
+		int n2 = persons[1]; //living on the current floor
+		int n3 = 0; //people living above of the current floor
+		
+		int targetFloor = 1;
+		
+		for(int i = 2; i < persons.length; i++) {
+			n3 +=  persons[i];
+			minFloor = minFloor + (i-1)*persons[i];
+		}
+		
+		
+		
+		//stop at the i-thh floor, starting from 2
+		for(int i = 2; i < persons.length; i++) {
+			if(n1 + n2 < n3) {
+				
+				targetFloor = i;
+				minFloor = (n1 + n2 - n3);
+				n2 = persons[i];
+				n3 -= persons[i];
+				
+			} else {
+				break;
+			}
+		}
+		
+		System.out.println("target floor: " + targetFloor);
+		
+		return minFloor;
+    }
+	
+}
