86c193e3151c7687764cbf0d97099a721927c699
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-24 21:14:34 +0000

    precomputing rectanges

---
+++ trunk/leetcode/flginterviews/PrecomputeRectangles.java
@@ -0,0 +1,9 @@
+
+public class PrecomputeRectangles {
+
+	//given two coordinates, return the size of rectange of it
+	//so only compute the retangle of each point to the top-left corner
+	//then do simple math.
+	//see: http://www.ardendertat.com/2011/09/20/programming-interview-questions-2-matrix-region-sum/
+	
+}
