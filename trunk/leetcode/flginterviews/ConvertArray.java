c0eb222c9cca66869f97d0df39ec8ea70e6d0bb9
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-24 21:12:03 +0000

    questions

---
+++ trunk/leetcode/flginterviews/ConvertArray.java
@@ -0,0 +1,22 @@
+/**
+ * Given an array [a1, a2, ..., aN, b1, b2, ..., bN, c1, c2, ..., cN] 
+ * convert it to [a1, b1, c1, a2, b2, c2, ..., aN, bN, cN] in-place
+ * using constant extra space - See more at:
+ * http://www.ardendertat.com/2012/01/09/programming-interview-questions/#sthash.dFMldWJu.dpuf
+ * */
+public class ConvertArray {
+
+	/**
+	 * def getIndex(currentIndex, N): return (currentIndex%3)*N + (currentIndex/3)
+	 * 
+	 * 
+	 * def convertArray(arr): N=len(arr)/3 for currentIndex in range(len(arr)): swapIndex=getIndex(currentIndex, N) while swapIndex
+	 * 
+	 * */
+	
+	//total number of each
+	//http://www.ardendertat.com/2011/10/18/programming-interview-questions-9-convert-array/
+	public static int getIndex(int currIndex, int totalNum) {
+		return currIndex
+	}
+}
