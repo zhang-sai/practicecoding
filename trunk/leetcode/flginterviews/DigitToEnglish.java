19e69e0eec721748d440d681e892d70bf0846ca9
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-24 20:23:35 +0000

    digit to english

--- trunk/leetcode/flginterviews/DigitToEnglish.java
+++ trunk/leetcode/flginterviews/DigitToEnglish.java
@@ -1,7 +1,54 @@
 /**
  *  convert number to english e.g 101 -> one hundred and one
+ *  
+ *  less than six digits
  * */
-xx
+
 public class DigitToEnglish {
 
+	static String[] lessThanTen = new String[]{"zero", "one", "two", "three", "four", "five", "six",
+			"seven", "eight", "nine"};
+	static String[] tens = new String[] {"", "ten", "twenty", "thirty", "fourty",
+			"fifty", "sixty", "seventy", "eighty", "ninety"};
+	static String[] tenToTwenty = new String[] {"ten", "eleven", "twelve", "thirteen", "fourteen",
+			"fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	static String[] hundreds = new String[]{"hundred", "thousand"};
+	
+	public static String say(int number) {
+		int thousands = number/1000;
+		int rest = number - 1000*thousands;
+		String result = "";
+		if(thousands > 0) {
+			result += sayLessThousand(thousands) + " thousand ";
+		}
+		if(rest > 0 && result.length() > 0) {
+			result += result + " and ";
+		}
+		result += sayLessThousand(rest);
+		return result;
+	}
+	
+	public static String sayLessThousand(int number) {
+		if(number < 10) {
+			return lessThanTen[number];
+		} else if (number < 20) {
+			return tenToTwenty[number-10];
+		} else if (number < 100) {
+			int num = number/10;
+			int rest = number - 10*num;
+			return tens[num] + (rest == 0 ? "" : " " + sayLessThousand(rest));
+		} else {
+			int num = number/100;
+			int rest = number - 100*num;
+			return lessThanTen[num] + " hundred" + (rest == 0 ? "" : " and " + sayLessThousand(rest));
+		}
+	}
+	
+	public static void main(String[] args) {
+//		for(int i = 0; i < 100; i++) {
+//		    System.out.println(say(i));
+//		}
+		System.out.println(say(987));
+	}
+	
 }
