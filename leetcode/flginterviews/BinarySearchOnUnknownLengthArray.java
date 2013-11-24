9eb70b6e5a11eac2808073364df7ba098dae4028
racezhang <racezhang@gmail.com>
2013-11-24 20:42:53 +0000

    binary search

---
+++ leetcode/flginterviews/BinarySearchOnUnknownLengthArray.java
@@ -0,0 +1,9 @@
+
+public class BinarySearchOnUnknownLengthArray {
+
+	//search for the right part via: 0, 2, 4, 8, 16, ..., 32
+	//until either an exception occurs, or the number is > the target
+	
+	//the rest is the standard binary search
+	
+}
