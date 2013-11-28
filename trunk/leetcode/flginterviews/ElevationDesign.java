284b9055f15dc2ac23fd96e970cbf05898bc2b06
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-28 03:50:40 +0000

    elevator design

--- trunk/leetcode/flginterviews/ElevationDesign.java
+++ trunk/leetcode/flginterviews/ElevationDesign.java
@@ -1,5 +1,28 @@
 
-xx
+//credit goes to:
+//http://stackoverflow.com/questions/493276/modelling-an-elevator-using-object-oriented-analysis-and-design
+
+//more details: http://www.cs.cmu.edu/~luluo/Courses/18540PhDreport.pdf
 public class ElevationDesign {
 
+}
+
+enum Status {UP, DOWN, STAND, MAINTENANCE};
+
+class Elevator {
+	
+	Status status;
+	
+	int currentFloor;
+	
+	public void scheduleToFloor(int floor) {}
+	
+	public void goUp() {}
+	public void goDown() {}
+	
+	public void openDoor() {}
+	
+	public void closeDoor() {}
+	
+	public void alarm() {}
 }
