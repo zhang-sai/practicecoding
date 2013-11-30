4d14b4380270af6348002383e6a5f464677cbe6d
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-30 23:38:23 +0000

    add one more

---
+++ trunk/leetcode/flginterviews/IntervalTreeAndQueryStream.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * using interval tree to implement video stream, every block has a size:
+ 10 10 10 10 10 6  7
+ Q: where is the size 50, return the 6th
+ http://www.dgp.utoronto.ca/people/JamesStewart/378notes/22intervals/
+ * */
+xx
+public class IntervalTreeAndQueryStream {
+
+}
--- trunk/leetcode/flginterviews/NumberOfSwapsToMakeASortedList.java
+++ trunk/leetcode/flginterviews/NumberOfSwapsToMakeASortedList.java
@@ -1,7 +1,22 @@
 /**
  * How many pairs are swapped in a list
  * */
-x
+
+
+//swap to make another list:
+//http://stackoverflow.com/questions/2987605/minimum-number-of-swaps-needed-to-change-array-1-to-array-2
+
+//find the Longest Increasing Striing, and swap the remainings
+//what about 5, 4, 2, 3, 1
+// ==> 1, 4, 2, 3, 5
+// => 1, 3, 2, 4, 5
+//==> 1, 2, 3, 4, 5
+
+//this is not true
+//1, 5, 3, 4 ,2  => just one swap, even thoug the LIS length is 4
+
+@Deprecated
+//seems to be too difficult
 public class NumberOfSwapsToMakeASortedList {
 
 }
