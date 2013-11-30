31e475dd0a4865fd649bdd0aac757e0f7ad71e68
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-11-30 23:38:03 +0000

    min strength

--- trunk/leetcode/flginterviews/MinInitialStrength.java
+++ trunk/leetcode/flginterviews/MinInitialStrength.java
@@ -18,49 +18,73 @@ public class MinInitialStrength {
 		int row = grid.length;
 		int col = grid[0].length;
 		
+		//reaching the current position
 		int[][] mins = new int[row][col];
+		//the historical min value in the path
+		int[][] historyMins = new int[row][col];
 		
+		//min keeps the minimal energy of ALL paths reaching
+		//that point
 		mins[0][0] = grid[0][0];
+		historyMins[0][0] = grid[0][0];
+		
 		for(int i = 1 ; i < col; i++) {
-			mins[0][i] = Math.min(mins[0][i-1], mins[0][i-1] + grid[0][i]);
+			mins[0][i] = mins[0][i-1] + grid[0][i];
+			historyMins[0][i] = Math.min(mins[0][i-1], mins[0][i-1] + grid[0][i]);
 		}
 		for(int j = 1; j < row; j++) {
-			mins[j][0] = Math.min(mins[j-1][0], mins[j-1][0] + grid[j][0]);
+			mins[j][0] = mins[j-1][0] + grid[j][0]; 
+			historyMins[j][0] = Math.min(mins[j-1][0], mins[j-1][0] + grid[j][0]);
 		}
+		
 		//then perform dynamic programming
 		for(int r = 1; r < row; r++) {
 			for(int c = 1; c < col; c++) {
-				int minFromTop = Math.min(mins[r-1][c], mins[r-1][c] + grid[r][c]);
-				int minFromLeft = Math.min(mins[r][c-1], mins[r][c-1] + grid[r][c]);
-				//this is the max
-				mins[r][c] = Math.max(minFromTop, minFromLeft);
-				if(grid[r][c] > 0) {
-					mins[r][c] = grid[r][c] + mins[r][c];
+				//a path from the top
+				int minFromTop = mins[r-1][c] + grid[r][c]; //Math.min(mins[r-1][c], mins[r-1][c] + grid[r][c]);
+				int historyMinFromTop = Math.min(historyMins[r-1][c], historyMins[r-1][c] + grid[r][c]);
+				//a path from the left
+				int minFromLeft = mins[r][c-1] + grid[r][c]; //Math.min(mins[r][c-1], mins[r][c-1] + grid[r][c]);
+				int historyMinFromLeft = Math.min(historyMins[r][c-1], historyMins[r][c-1] + grid[r][c]);
+				
+				if(historyMinFromLeft > historyMinFromTop) {
+					historyMins[r][c] = historyMinFromLeft;
+					mins[r][c] = minFromLeft;
+				} else {
+					historyMins[r][c] = historyMinFromTop;
+					mins[r][c] = minFromTop;
 				}
 			}
 		}
 	
 		for(int i = 0; i < row; i++) {
 			for(int j = 0; j < col; j++) {
-				System.out.print(mins[i][j] + "    ");
+				System.out.print(historyMins[i][j] + "    ");
 			}
 			System.out.println();
 		}
 		
-		return mins[row-1][col-1];
+		return historyMins[row-1][col-1];
 	}
 	
 	public static void main(String[] args) {
 		
-		xx this produces the wrong answer
 		int[][] grid = new int[][] {
 			new int[]{-2, -1, 5, -99, 200},
 			new int[]{1,  2,  4, -60, 300},
 			new int[]{2,  -3,  -1, 2,  -99},
 			new int[]{-99, 200, 2, 1,  5}
 		};
-		System.out.println(getMin(grid));
+		System.out.println("Result: " + getMin(grid));
+		System.out.println("-----------");  //should be: -2
 		
+		grid = new int[][] {
+				new int[]{-2, -3, 5, -99, 200},
+				new int[]{1,  -1,  4, -60, 300},
+				new int[]{2,  -3,  -1, 2,  -99},
+				new int[]{0, 200, -199, 1,  5}
+			};
+		System.out.println("Result: " + getMin(grid)); //should be -2
 	}
 	
 }
