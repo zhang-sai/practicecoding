0e4a9156128478949b1a1fc1f0f61fdbd8dcbf72
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-12-04 22:08:14 +0000

    one more question

---
+++ trunk/leetcode/flginterviews/LongestCommonSubstring.java
@@ -0,0 +1,12 @@
+/**
+ * not subsequence. the character must be next to each other
+ * fsfsfABCDEFGsfff
+ * fsfABCDEFGsf
+ * is ABCDEFG
+ * 
+ * http://en.wikipedia.org/wiki/Longest_common_substring_problem
+ * */
+xx
+public class LongestCommonSubstring {
+
+}
