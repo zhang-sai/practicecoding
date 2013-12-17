0b1dfd2e95c03902876e8616b8bf023b5d332bbb
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-12-17 12:28:42 +0000

    factorial sum

--- trunk/leetcode/src/basics/FactorSum.java
+++ trunk/leetcode/src/basics/FactorSum.java
@@ -1,5 +1,7 @@
 package basics;
 
+import java.math.BigInteger;
+
 /**
  * 10!= 3628800
  * returns 3+6+2+8 + 8
@@ -7,24 +9,27 @@ package basics;
 public class FactorSum {
 
 	public static void main(String[] args) {
-		System.out.println(factorSum(100));
+		System.out.println(factorSum(10000));
 	}
 	
 	public static int factorSum(int v) {
-		int mul = 1;
-		for(int i = 1; i <= v; i++) {
-			mul = mul*i;
-			while(mul % 10 == 0) {
-				mul = mul / 10;
+		BigInteger ten = BigInteger.valueOf(10);
+		BigInteger r = BigInteger.valueOf(1);
+		for(int i = 1; i <=v; i++) {
+			BigInteger bi = BigInteger.valueOf(i);
+			r = r.multiply(bi);
+			while(r.divideAndRemainder(ten)[1].intValue() == 0) {
+				r = r.divide(ten);
 			}
 		}
-		System.out.println(mul);
+		
+		System.out.println(r);
 		int sum = 0;
-		while(mul/10 != 0) {
-			sum += mul%10;
-			mul = mul / 10;
+		while(r.divide(ten).intValue() != 0) {
+			sum += r.divideAndRemainder(ten)[1].intValue();
+			r = r.divide(ten);;
 		}
-		sum += mul;
+		sum += r.intValue();
 		
 		return sum;
 	}
