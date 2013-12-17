0a58dae2563f9125b699015f3447ac5185866602
racezhang <racezhang@gmail.com>
2013-12-17 12:30:38 +0000

    remove graph node

---
+++ leetcode/src/basics/GraphNode.java
@@ -0,0 +1,22 @@
+package basics;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class GraphNode {
+	static int count = 0;
+	public final int nodeId;
+	int val;
+	public List<GraphNode> nextNodes = new LinkedList<GraphNode>();
+	public GraphNode(int v) {
+		val = v;
+		nodeId = count++;
+	}
+	public GraphNode(int v, GraphNode[] nodes) {
+		nodeId = count;
+		count = count+ 1;
+		val = v;
+		nextNodes.addAll(Arrays.asList(nodes));
+	}
+}
