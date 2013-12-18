a82aba2579884a76f1df9d78cfe4934d27c99891
racezhang <racezhang@gmail.com>
2013-12-18 07:00:34 +0000

    questions

---
+++ leetcode/flginterviews/fb/AllCombinationSum.java
@@ -0,0 +1,14 @@
+package fb;
+
+/**
+ * Given a number N, write a program that returns all possible combinations of numbers that add up to N, as lists. (Exclude the N+0=N) 
+
+For example, if N=4 return {{1,1,1,1},{1,1,2},{2,2},{1,3}}
+
+http://www.careercup.com/question?id=6321181669982208
+ * 
+ * */
+x
+public class AllCombinationSum {
+
+}
