3de5d2cb7d42eef278ad819fdf25db297eaaf32b
racezhang <racezhang@gmail.com>
2013-12-18 04:39:17 +0000

    treenode

--- leetcode/flginterviews/linkedin/TreeNode.java
+++ leetcode/flginterviews/linkedin/TreeNode.java
@@ -6,7 +6,7 @@ public class TreeNode {
      public int val;
      public TreeNode left;
      public TreeNode right;
-     TreeNode(int x) { val = x; }
+     public TreeNode(int x) { val = x; }
      public String toString() {return val + ""; }
      
      int elem = 0;
