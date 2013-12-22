fde8d8e81aeef61742293f4afa5a6ace76db1264
racezhang <racezhang@gmail.com>
2013-12-22 13:04:53 +0000

    delete nodes in bst

--- leetcode/src/basics/DeleteNodeInBinaryTree.java
+++ leetcode/src/basics/DeleteNodeInBinaryTree.java
@@ -5,7 +5,7 @@ import linkedin.TreeNode;
 
 //http://www.algolist.net/Data_structures/Binary_search_tree/Removal
 
-xx
+
 public class DeleteNodeInBinaryTree {
 
 //	static class TreeNode {
@@ -50,14 +50,14 @@ public class DeleteNodeInBinaryTree {
 		}
 		//did not find it
 		if(node == null) {
+			System.out.println("Canot find the node with value: " + val);
 			return root;
 		}
 		//find the value and need to delete it
 		if(node.left == null && node.right == null) {
 			if(parent == null) {
 				//delete the root
-				root = null;
-				return root;
+				return null;
 			} else if(parent.left == node) {
 				parent.left = null;
 			} else {
@@ -74,93 +74,78 @@ public class DeleteNodeInBinaryTree {
 				parent.right = node.left == null ? node.right : node.left;
 			}
 		} else {
-			//has two children
-			//find the min value in the right tree
-			TreeNode rightSub = node.right;
-			TreeNode rightParent = node;
-			int min = rightSub.val;
-			while(rightSub != null) {
-				if(rightSub.left != null) {
-					rightParent = rightSub;
-					rightSub = rightSub.left;
-					min = rightSub.val;
-				}
-			}
-			//get the min
+			//if the node has two
+			int min = findMin(node.right);
+			delete(root, min);
 			node.val = min;
-			//disconnect the min from the rightSub
-			if(rightSub.right != null) {
-				if(rightParent.right == rightSub) {
-					rightParent.right = rightSub.right;
-				} else {
-					rightParent.right = rightSub.left;
-				}
-			} else {
-				if(rightParent.right == rightSub) {
-					rightParent.right = null;
-				} else {
-					rightParent.left = null;
-				}
-			}
 		}
 		
 		return root;
 	}
 	
+	private static int findMin(TreeNode node) {
+		if(node.left == null) {
+			return node.val;
+		} else {
+			return findMin(node.left);
+		}
+	}
+	
 	public static void main(String[] args) {
-		TreeNode root = new TreeNode(0);
+		TreeNode n3 = new TreeNode(3);
+		TreeNode n0 = new TreeNode(0);
 		TreeNode n1 = new TreeNode(1);
 		TreeNode n2 = new TreeNode(2);
-		TreeNode n3 = new TreeNode(3);
 		TreeNode n4 = new TreeNode(4);
 		TreeNode n5 = new TreeNode(5);
 		TreeNode n6 = new TreeNode(6);
 		
+		TreeNode root = n3;
+		
 		root.left = n1;
-		root.right = n2;
+		root.right = n6;
 		
-		n1.left = n3;
-		n1.right = n4;
+		n1.left = n0;
+		n1.right = n2;
 		
-		n2.left = n5;
-		n5.right = n6;
+		n6.left = n4;
+		n4.right = n5;
 		
 		/**
-		 *        0
+		 *        3
 		 *      /   \
-		 *     1     2
+		 *     1     6
 		 *    / \   /
-		 *   3   4  5
+		 *   0   2  4
 		 *           \
-		 *            6   
+		 *            5   
 		 * 
 		 * */
 		
 		System.out.println(Utils.serialize(root));
+		System.out.println();
 		
 		System.out.println("delete: " + 20);
-		
 		root = delete(root, 20);
 		System.out.println(Utils.serialize(root));
+		System.out.println();
 		
 		System.out.println("delete: " + 4);
-		
 		root = delete(root, 4);
 		System.out.println(Utils.serialize(root));
+		System.out.println();
 		
-		System.out.println("delete: " + 0);
-		
-		root = delete(root, 0);
+		System.out.println("delete: " + 3);
+		root = delete(root, 3);
 		System.out.println(Utils.serialize(root));
 		
 		System.out.println("delete: " + 5);
-		
 		root = delete(root, 5);
 		System.out.println(Utils.serialize(root));
+		System.out.println();
 		
-		System.out.println("delete: " + 1);
-		
-		root = delete(root, 1);
+		System.out.println("delete: " + 6);
+		root = delete(root, 6);
 		System.out.println(Utils.serialize(root));
 	}
 	
