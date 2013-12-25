dca33f69384a7e5f5feafd3cbb259f95c994a38b
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-12-25 13:54:38 +0000

    evaluate expression

--- trunk/leetcode/flginterviews/linkedin/EvaluateExper.java
+++ trunk/leetcode/flginterviews/linkedin/EvaluateExper.java
@@ -8,15 +8,82 @@ import java.util.Stack;
  * 4 + 2*4 = 12
  * */
 public class EvaluateExper {
-x
+
 	public static int evaluateExpr(String expr) {
-		Stack<Character> stack = new Stack<Character>();
-		int result = 0;
+		Stack<Character> opStack = new Stack<Character>();
+		Stack<Integer> numStack = new Stack<Integer>();
+		
+//		int result = 0;
 		for(char c : expr.toCharArray()) {
-			
+			if(isDigit(c)) {
+				numStack.push(c - '0');
+			} else if (isSign(c)) {
+				if(opStack.isEmpty()) {
+					opStack.push(c);
+				} else {
+					//check the first character in the peek
+					char prevSign = opStack.peek();
+					if(getPred(prevSign) >= getPred(c)) {
+						opStack.pop(); //pop the previous sign
+						int v1 = numStack.pop();
+						int v2 = numStack.pop();
+						int v = compute(prevSign, v2, v1);
+						numStack.push(v);
+						opStack.push(c);
+					} else {
+						opStack.push(c);
+					}
+				}
+			} else {
+				throw new Error("");
+			}
 		}
+		
+		//compute the result
+		int result = 0;
+		if(opStack.isEmpty()) {
+			result = numStack.pop(); //get the result
+		} else {
+			while(!opStack.isEmpty()) {
+				int v1 = numStack.pop();
+				int v2 = numStack.pop();
+				char sign = opStack.pop();
+				int v = compute(sign, v2, v1);
+				result = v;
+				numStack.push(v);
+			}
+		}
+		
+		
 		return result;
 	}
+
+   static int compute(char sign, int v1, int v2) {
+	   if(sign == '+') {
+		   return v1+v2;
+	   } else if (sign == '-') {
+		   return v1 - v2;
+	   } else if (sign == '*') {
+		   return v1*v2;
+	   } else {
+		   return v1/v2;
+	   }
+   }
+
+    static boolean isDigit(char c) {
+    	return c >= '0' && c <= '9';
+    }
+    
+    static boolean isSign(char c) {
+    	return c == '+' || c == '-' || c == '*' || c== '/';
+    }
+    
+    static int getPred(char c) {
+    	if(!isSign(c)) {
+    		throw new Error();
+    	}
+    	return c=='+' || c == '-' ? 1 : 2;
+    }
 	
 	public static void main(String[] args) {
 		String expr = "4+2-3";
