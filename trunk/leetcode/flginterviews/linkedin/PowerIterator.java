cabb2ab05151ac168a9297b4f92dbf696f1db929
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2013-12-28 06:15:21 +0000

    power iterator

--- trunk/leetcode/flginterviews/linkedin/PowerIterator.java
+++ trunk/leetcode/flginterviews/linkedin/PowerIterator.java
@@ -4,101 +4,128 @@ import java.util.Iterator;
 import java.util.PriorityQueue;
 
 /**
- * Provide an implementation of the following interface: 
-
-http://www.careercup.com/question?id=8591375
-
+ * Provide an implementation of the following interface:
+ * 
+ * http://www.careercup.com/question?id=8591375
+ * 
  * */
 public class PowerIterator {
 
-	public static interface Powers extends Iterator<Long> 
-	{ 
-	/* Returns the next integer a in the arithmetic sequence of integers where 
-	* a = m^n, m > 1 n > 1, and m and n are both integers 
-	* Thus, the first few outputs will be 4, 8, 9, 16, 25, 27, 32, 36, etc. 
-	*/ 
+	public static interface Powers extends Iterator<Long> {
+		/*
+		 * Returns the next integer a in the arithmetic sequence of integers
+		 * where a = m^n, m > 1 n > 1, and m and n are both integers Thus, the
+		 * first few outputs will be 4, 8, 9, 16, 25, 27, 32, 36, etc.
+		 */
 
-		//2^2, 2^3, 3^2, 2^4 (4^2), 5^2, 3^3, ...
-		
-	public Long next(); 
+		// 2^2, 2^3, 3^2, 2^4 (4^2), 5^2, 3^3, ...
 
-	/* Resets the sequence to the beginning, such that the next call to next() 
-	* will return 4. 
-	*/ 
-	public void reset(); 
+		public Long next();
+
+		/*
+		 * Resets the sequence to the beginning, such that the next call to
+		 * next() will return 4.
+		 */
+		public void reset();
 	}
-	
+
 	public static class PIter implements Powers {
-		
-		   private long next = 4;  
-		   private int base = 2;  
 
-		   /* b = logn/loga */
-//		   private int isPerfectPower(long n) {  
-//		     for (int a=2; a<n/2; ++a) {  
-//		       double b = Math.log(n) / Math.log(a);  
-//		       if (b - (int)b == 0) return a;  
-//		     }  
-//		     return -1;  
-//		   }  
-
-		   /* binary search */
-		   public int isPerfectPower(long n) {  
-		     for (int b=2; b<(int)(Math.log(n)/Math.log(2)+1); ++b) {  
-		       long l = 2, r = n/2;  
-		       while (l <= r) {  
-		         long mid = l + (r - l) / 2;  
-		         long v = (long)Math.pow(mid, b);  
-		         if (v == n) return (int)mid;  
-		         if (v > n) {  
-		           r = mid - 1;  
-		         } else {  
-		           l = mid + 1;  
-		         }  
-		       }  
-		     }  
-		     return -1;  
-		   }  
-
-		   @Override  
-		   public Long next() {  
-		     long pre = next;  
-		     next = next*base;  
-		     for (long i=pre+1; i<next; ++i) {  
-		       int a = isPerfectPower(i);  
-		       if (a > 0) {  
-		         next = i;  
-		         base = j;  
-		         break;  
-		       }  
-		     }  
-		     return pre;  
-		   }  
-		   
-		   @Override  
-		   public void reset() {  
-		     next = 4; base = 2;  
-		   }
+//		private long next = 4;
+		private NumPair curr = new NumPair(2, 2);
+			
+		@Override
+		public void reset() {
+			curr = new NumPair(2, 2);
+		}
 
 		@Override
 		public boolean hasNext() {
 			// TODO Auto-generated method stub
-			return false;
+			return true;
 		}
 
 		@Override
 		public void remove() {
 			// TODO Auto-generated method stub
-			
-		}  
+
+		}
+
+		@Override
+		public Long next() {
+			long retValue = curr.v;
+			//compute the next NumPair
+			this.computeNextValue();
+			return retValue;
+		}
 		
+		private void computeNextValue() {
+			int minValue = curr.v + 1;
+			int maxValue = (int) Math.min(Math.pow(curr.a + 1, curr.b),
+					Math.pow(curr.a, curr.b + 1));
+			for(int v = minValue; v <= maxValue; v++) {
+				NumPair np = isPerfectSquareNum(v);
+				if(np != null) {
+					this.curr = np;
+//					System.out.println(np);
+					break;
+				}
+			}
+		}
+
 	}
 	
+	static class NumPair {
+		final int a;
+		final int b;
+		final int v;
+		public NumPair(int a, int b) {
+			this.a = a;
+			this.b = b;
+			this.v = (int)Math.pow(a, b);
+		}
+		public String toString() {
+			return a + "^" + b + "=" + v;
+		}
+	}
 	
-	
+	//check whether it can be written as a^b = n
+	public static NumPair isPerfectSquareNum(int n) {
+		//b <= log2(n)
+		//a <= sqrt(n)
+		int bMax = (int)(Math.log(n)/Math.log(2));
+		int aMax = (int)Math.sqrt(n);
+//		System.out.println(bMax + "   " + aMax);
+		//check every b
+		for(int b = 2; b <= bMax; b++) {
+			//binary search a and compute a^b
+			int low = 2;
+			int high = aMax;
+			while(low <= high) {
+				int a = (low + high)/2;
+				int v = (int) Math.pow(a, b);
+				if(v == n) {
+//					System.out.println(a + "^" + b + "=" + n);
+					return new NumPair(a, b);
+				} else if (v > n) {
+					high = a - 1;
+				} else {
+					low = a + 1;
+				}
+			}
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
+		for(int i = 5; i < 100; i++) {
+			NumPair p =  isPerfectSquareNum(i);
+			if(p != null) {
+				System.out.println(p);
+			}
+		}
 		PIter p = new PIter();
-		for(int i = 0; i < 10; i++) {
+		for (int i = 0; i < 1000; i++) {
 			System.out.println(p.next());
 		}
 	}
