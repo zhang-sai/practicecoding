601ad2b659477eea1a1fb11005924b1fa3844379
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-01-29 06:39:48 +0000

    meeting arrangement

---
+++ trunk/leetcode/flginterviews/fb/MaxMeetingArrangement.java
@@ -0,0 +1,15 @@
+package fb;
+
+/**
+ * A number of events for a day,  starting time and end time
+find a solution that maximizes the number of events 
+one can finish in the day. Events may over lap with each other.
+
+http://www.mitbbs.com/bbsann2/life.faq/JobHunting/mianshiexp/D12845078562O0/M.1293838143_2.Z0/Facebook+2+%E8%BD%AE%E7%94%B5%E9%9D%A2%E9%9D%A2%E7%BB%8F+%2B+%E4%B8%BA%E7%AC%AC%E4%B8%89%E8%BD%AE%E6%B1%82%E7%A6%8F
+ * */
+public class MaxMeetingArrangement {
+
+	//see the max job cost:
+	//MaxJobCosts
+	
+}
