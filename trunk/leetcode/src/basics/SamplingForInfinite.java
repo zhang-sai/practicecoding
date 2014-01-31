bf88e641a8218be30666e1b326a9c06760d53b4d
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-01-31 17:07:35 +0000

    reservoir sampling

---
+++ trunk/leetcode/src/basics/SamplingForInfinite.java
@@ -0,0 +1,24 @@
+package basics;
+
+/**
+ * reservoir sampling for infinite stream,
+ * */
+
+/**
+ * Just like shuffling a card:
+ * 
+ * It can be solved in O(n) time. The solution also suits well for input in the form of stream. The idea is similar to this post. Following are the steps.
+
+1) Create an array reservoir[0..k-1] and copy first k items of stream[] to it.
+2) Now one by one consider all items from (k+1)th item to nth item.
+…a) Generate a random number from 0 to i where i is index of current item in stream[]. Let the generated random number is j.
+…b) If j is in range 0 to k-1, replace reservoir[j] with arr[i]
+ * 
+ * see here:
+ * http://www.geeksforgeeks.org/reservoir-sampling/
+ * */
+
+//explanation: http://code-slim-jim.blogspot.com/2010/06/reservoir-sampling.html
+public class SamplingForInfinite {
+
+}
