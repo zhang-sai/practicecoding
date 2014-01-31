fa0912efb50f9523246acba98a96189d0afacc3d
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-01-31 00:33:29 +0000

    missing num

--- trunk/leetcode/flginterviews/MissingNumbers.java
+++ trunk/leetcode/flginterviews/MissingNumbers.java
@@ -3,6 +3,10 @@
  * except one. Find the missing number. What if there are two numbers
  * missing. How many missing numbers can you find using different algorithms?
  * */
+
+//find two missing number in N numbers in range [0, N].
+
+@Deprecated
 public class MissingNumbers {
 
 	//using the xor
