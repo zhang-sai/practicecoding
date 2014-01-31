b775d0e429fa303da8f0da1cef8fed7a61a7175f
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-01-31 18:46:43 +0000

    minimal edge

---
+++ trunk/leetcode/src/basics/MinEdgeCoverage.java
@@ -0,0 +1,13 @@
+package basics;
+
+/**
+ * min edge coverage by vertex
+ * - use edge to cover vertex is poly
+ * - use vertex to cover edge is npc
+ * */
+@Deprecated
+public class MinEdgeCoverage {
+
+	//reduce to bipartie diagram
+	//and then do greedily
+}
