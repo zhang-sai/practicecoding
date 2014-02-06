afdb4a6b1e6e0c046896df3f31e988f87a55f104
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-02-06 04:28:59 +0000

    min weight

---
+++ trunk/leetcode/flginterviews/MinWeightTwoGroups.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+
+/**
+ * there are two groups of people; n number of A's & n of B's. We want to have n pairs of A and B from two groups and  to minimize the sum of the weight differences.
+EG
+
+        A[]={5,3,4};
+        B[]={1,6,2}; 
+min = 5
+3-1, 4-2,5-6
+ * 
+ * */
+public class MinWeightTwoGroups {
+
+	public static void main(String[] args) {
+		System.out.println(minWeight(new int[]{5, 3, 4}, new int[]{1, 6, 2}));
+	}
+	
+	public static int minWeight(int[]a, int[]b){
+	    int dp[][] = new int[a.length+1][b.length+1];
+	    Arrays.sort(a);
+	    Arrays.sort(b);
+	    
+	    for(int i =1;i<dp.length;i++) { 
+	    	dp[i][0]=Integer.MAX_VALUE;
+	    }
+	    
+	    for(int i =1;i<dp[0].length;i++) {
+	    	dp[0][i]=Integer.MAX_VALUE;  
+	    }
+	    
+	    dp[0][0]=0;
+	    for(int i =1;i<dp.length;i++)
+	        for(int j=1;j<dp[0].length;j++){
+	            int temp=Math.min(dp[i-1][j], dp[i][j-1]);
+	            dp[i][j] = Math.abs(a[i-1]-b[j-1]) + Math.min(dp[i-1][j-1],temp);
+	        }
+	    return dp[a.length][b.length];
+	}
+}
