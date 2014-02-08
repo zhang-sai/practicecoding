7d6c7f63d110f69272d0a546f921505007a2169d
racezhang <racezhang@gmail.com>
2014-02-08 23:33:17 +0000

    push pop number sequences

--- leetcode/flginterviews/g/PushPopNumberSequences.java
+++ leetcode/flginterviews/g/PushPopNumberSequences.java
@@ -1,6 +1,5 @@
 package g;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -26,22 +25,35 @@ import java.util.Stack;
  * Input: 5,1,4,2,3 Output (some sequences are impossible): impossible
  * 
  * */
+
 public class PushPopNumberSequences {
 
+	//credit goes to: http://www.mitbbs.com/article_t/JobHunting/32613553.html
+	
+	/**
+	 * Basic algorithm:
+	 * 
+	 * */
 	static List<String> getOps(String seq) {
 		List<String> v = new LinkedList<String>();
 		int n = seq.length();
 
+		//map from integer_value to its index
 		Map<Integer, Integer> pos = new HashMap<Integer, Integer>();
 		for (int i = 0; i < n; i++) {
 			pos.put(seq.charAt(i) - '0', i);
 		}
 
+		//create a stack to mimic the whole process
 		Stack<Integer> stack = new Stack<Integer>();
+		//i is the value which is [1, n]
+		//j is the index in the output result
 		for (int i = 1, j = 0; i <= n; i++) {
+			//Important: the stack top should be popped later than the current i
 			if (stack.empty() || pos.get(stack.peek()) > pos.get(i)) {
 				stack.push(i);
 				v.add("push_back");
+			//Important: the stack bottom is popped earlier than the current i, so push front
 			} else if (pos.get(stack.get(0)) < pos.get(i)) {  //mimic the front
 				stack.add(0, i);  //mimic the front
 				v.add("push_front");
@@ -50,6 +62,7 @@ public class PushPopNumberSequences {
 				break;
 			}
 
+			//check whether the stack can start pop to meet the current j
 			while (!stack.empty() && stack.peek() == seq.charAt(j) - '0') {
 				stack.pop();
 				v.add("pop_back");
