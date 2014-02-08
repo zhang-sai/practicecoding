35f13eb41eb499dc3228df2f4887fe9a8c36b13c
racezhang <racezhang@gmail.com>
2014-02-08 00:55:56 +0000

    remove duplication lines

---
+++ leetcode/flginterviews/g/RemoveLineDuplication.java
@@ -0,0 +1,23 @@
+package g;
+
+/**
+ * remove duplicate lines of a file(what if the file is very large which could 
+not be held in the main memory)
+ * */
+public class RemoveLineDuplication {
+
+	/**
+	 * 1. do external sort, and check it
+	 * 
+	 * 2. hash each line to different memory location, and then sort
+	 * 
+	 * 3. then do hash code
+	 * 
+	 * 4. approximately, use bloom filters or count-min
+	 * */
+	//1. 
+	//external sort
+	
+	//hash and then finger print
+	
+}
