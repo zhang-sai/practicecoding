de251048808580c228d1146b6375bccb3d43bc5b
racezhang <racezhang@gmail.com>
2014-02-08 19:07:46 +0000

    interval tree

---
+++ leetcode/flginterviews/fb/IntervalTreeImpl.java
@@ -0,0 +1,30 @@
+package fb;
+
+//implement the function of insert, and find
+//
+//cannot use sorted array, which cannot support dynamic updating
+//http://en.wikipedia.org/wiki/Interval_tree
+
+//one example:
+//http://algs4.cs.princeton.edu/93intersection/IntervalST.java.html
+
+/**
+ * 
+ * using interval tree to implement video stream, every block has a size:
+ 10 10 10 10 10 6  7
+ Q: where is the size 50, return the 6th
+ http://www.dgp.utoronto.ca/people/JamesStewart/378notes/22intervals/
+ 
+ This is also good too:https://www.cs.umd.edu/class/spring2008/cmsc420/L22.IntervalTrees.pdf
+ * */
+
+//Difficult
+
+@Deprecated
+public class IntervalTreeImpl {
+
+	
+	//insert, and find
+	
+	
+}
