ef4b65ba1b81d64419ad87c61fbed1cbb9678c4b
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-02-08 00:43:24 +0000

    find the common words

---
+++ trunk/leetcode/flginterviews/linkedin/CommonWords.java
@@ -0,0 +1,31 @@
+package linkedin;
+
+//http://www.mitbbs.com/article_t/JobHunting/32619691.html
+public class CommonWords {
+
+	/**
+	 * This is a good day
+       This is a bad day
+       That was good day
+       
+       return the first / second sentences, since they share the
+       most number of common words
+	 * */
+	
+			
+			/**
+			 * 1. use reverse index to keep track of the word and sentence
+			 *    e.g.,
+			 *    This --> {sentence1, sentence2}
+			 *    
+			 *    use an array to store all intersections array[][]
+			 *    that is: array[sentence1][sentence]++;
+			 *    
+			 *    finally figure out the largest value
+			 *    
+			 * 2. bit vector, in Java, it is BitSet
+			 * 
+			 *   for brute force comparison
+			 * 
+			 * */
+}
