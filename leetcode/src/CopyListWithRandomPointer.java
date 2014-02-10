c6d1ef655912f863a237aeddc3671058fc6fba4a
racezhang <racezhang@gmail.com>
2014-02-10 20:29:41 +0000

    copy list with random pointers

--- leetcode/src/CopyListWithRandomPointer.java
+++ leetcode/src/CopyListWithRandomPointer.java
@@ -6,10 +6,95 @@ import java.util.Map;
 
 Return a deep copy of the list.
  * */
-x
+
 public class CopyListWithRandomPointer {	
 	
+	public static void main(String[] args) {
+		RandomListNode n1 = new RandomListNode(1);
+		RandomListNode n2 = new RandomListNode(2);
+		RandomListNode n3 = new RandomListNode(3);
+		RandomListNode n4 = new RandomListNode(4);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		
+		n1.random = n3;
+		n2.random = n4;
+		
+		RandomListNode copied = copyRandomListWithConstantSpace(n1);
+		
+//		System.out.println(copied.label);
+//		System.out.println(copied.next.label);
+//		System.out.println(copied.next.next.label);
+//		System.out.println(copied.next.next.next.label);
+		
+		while(copied != null) {
+			System.out.println(copied.label + " " + (copied.random != null ? copied.random.label : "null"));
+//			+ ", random: "
+//					+ copied.random != null ? copied.random.label : "null");
+			copied = copied.next;
+		}
+		
+	}
+	
+	/**
+	 * With constant space
+	 * page 128
+	 * */
+	public static RandomListNode copyRandomListWithConstantSpace(RandomListNode head) {
+		
+		if(head == null) {
+			return null;
+		}
+		RandomListNode newBeforeHead = new RandomListNode(-1);
+		
+		//copy the old node to a new node, fill in the data, copy the newnode.next
+		//make oldnode.next = new node, make the newnode.random un-set
+		RandomListNode oldCurr = head;
+//		RandomListNode newPrev = newBeforeHead;
+		while(oldCurr != null) {
+			RandomListNode newCurr = new RandomListNode(oldCurr.label);
+			newCurr.next = oldCurr.next;
+			
+			//RandomListNode next = oldCurr.next;
+			oldCurr.next = newCurr;
+			
+			//set the new node
+			if(newBeforeHead.next == null) {
+				System.out.println("set: " + newCurr.label);
+				newBeforeHead.next = newCurr;
+			}
+			
+			//set the old curr
+			oldCurr = newCurr.next;
+		}
+		
+		
+		//make newnode.random to the right place
+		oldCurr = head;
+		while(oldCurr != null) {
+			//oldCurr.next is the new
+			oldCurr.next.random = oldCurr.random != null ? oldCurr.random.next : null;
+			oldCurr = oldCurr.next.next; //must be two next
+		}
+		
+		//reset the old.next
+		oldCurr = head;
+		while(oldCurr != null) {
+			oldCurr.next = oldCurr.next.next;
+			oldCurr = oldCurr.next;
+		}
+		
+		
+		System.out.println("return: " + newBeforeHead.next.label);
+		return newBeforeHead.next;
+	}
+	
 
+	/**
+	 * With O(n) space
+	 * */
 	public RandomListNode copyRandomList(RandomListNode head) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         
