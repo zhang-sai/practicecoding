ce2dc0823fdb7275f1f398c0883faf64e0e16cb2
racezhang <racezhang@gmail.com>
2014-02-11 22:40:22 +0000

    office location

---
+++ leetcode/flginterviews/g/OfficeLocation.java
@@ -0,0 +1,15 @@
+package g;
+
+/**
+ * find a intersection to build office so that the sum of all employees'
+commute distances is minimum.. (the map is represented as a m*n grid, you 
+are given each employee's coordination, they can only move in up-down and 
+left-right directions)
+ * */
+public class OfficeLocation {
+
+	//n^2 is an easy solution
+	
+	//use the median point as the location for both x and y coordinates
+	
+}
