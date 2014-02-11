41e59686e8bf5076cd0b38d90cb51226c10fc017
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-02-11 04:10:07 +0000

    min swap

---
+++ trunk/leetcode/flginterviews/g/MinSwap.java
@@ -0,0 +1,20 @@
+package g;
+
+/**
+ * 
+ * void minMSwap(int[] num, int m), return the min array after m swaps, each 
+swap happens only between two adjacent elements([4,2,1,3], 2 return [1,4,2,3
+])
+
+4,2,1,3
+4,1,2,3
+1,4,2,3
+ * */
+public class MinSwap {
+
+	//min swap
+	
+	//use recursion
+	
+	//first move the smallest to the head, then for the rest one, do recursion
+}
