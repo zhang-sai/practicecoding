604880dc10aac20e95fd04547b5cdfcda45a580f
racezhang <racezhang@gmail.com>
2014-02-17 17:02:28 +0000

    rectangle overlap

---
+++ leetcode/src/basics/TwoRectangleOveralp.java
@@ -0,0 +1,26 @@
+package basics;
+
+/**
+ * see here:
+ * http://leetcode.com/2011/05/determine-if-two-rectangles-overlap.html
+ * 
+ * Given two axis-aligned rectangles A and B. Write a function to determine if the two rectangles overlap
+ * */
+public class TwoRectangleOveralp {
+
+	/**
+	 * P1
+	 *  ------
+	 * |      |
+	 * |------|
+	 *        p2
+	 *        
+	 *        p3 ---------
+	 *           |       |
+	 *           ---------P4
+	 * 
+	 * ! ( P2.y < P3.y || P1.y > P4.y || P2.x < P3.x || P1.x > P4.x )
+	 * 
+	 * */
+	
+}
