2f08a035e1a18b242422dd3f6edcb7e443baa79e
racezhang <racezhang@gmail.com>
2014-02-17 05:52:15 +0000

    prime factor

--- leetcode/src/basics/PrimeFactors.java
+++ leetcode/src/basics/PrimeFactors.java
@@ -7,6 +7,8 @@ import java.util.List;
  * print out prime factors. e.g., 20=2x2x5, 90=2x3x3x5 How to get a list of
  * prime numbers
  * */
+
+//same see: FactorSum
 public class PrimeFactors {
 	
 	public static List<Integer> primeFactors(int number) {
