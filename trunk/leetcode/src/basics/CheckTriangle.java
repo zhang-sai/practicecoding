5a2e29ccf184c6f8cae64deee963fc223450577b
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-03-02 22:53:44 +0000

    count triangle numbers

--- trunk/leetcode/src/basics/CheckTriangle.java
+++ trunk/leetcode/src/basics/CheckTriangle.java
@@ -1,6 +1,7 @@
 package basics;
 
-x
+import java.util.Arrays;
+
 /**
  * given an unsorted integer array, find three integers can form a triangle
  * 
@@ -12,6 +13,48 @@ public class CheckTriangle {
 	 * a + b > c
        a + c > b
        b + c > a
-	 * 
 	 * */
+	
+	public static int countTriangles(int[] values) {
+		if(values.length < 3) {
+			return 0;
+		}
+		/**
+		 * First sort all the values
+		 * */
+		Arrays.sort(values);
+		int count = 0;
+		 
+	    // Fix the first element.  We need to run till n-3 as the other two elements are
+	    // selected from arr[i+1...n-1]
+	    for (int i = 0; i < values.length - 2; ++i)
+	    {
+	        // Initialize index of the rightmost third element
+	        int k = i+2;
+	 
+	        // Fix the second element
+	        for (int j = i+1; j < values.length; ++j)
+	        {
+	            // Find the rightmost element which is smaller than the sum
+	            while (k < values.length && values[i] + values[j] > values[k])
+	               k++;
+	 
+	            //ke can be just values.length
+	            //so that count is still correct
+	            count += k - j - 1;
+	        }
+	    }
+		
+		return count;
+		
+	}
+	
+	private static boolean isTriangle(int a, int b, int c) {
+		return a + b > c && b + c > a && a + c > b;
+	}
+	
+	public static void main(String[] args) {
+		int[] values = new int[]{10, 21, 22, 100, 101, 200, 300};
+		System.out.println("The number of triangles: " + countTriangles(values));
+	}
 }
