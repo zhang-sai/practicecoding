af150538b80e1a6c384d52f6584ac66a3d1b4e33
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-03-02 06:50:08 +0000

    triangle

---
+++ trunk/leetcode/src/basics/CheckTriangle.java
@@ -0,0 +1,17 @@
+package basics;
+
+x
+/**
+ * given an unsorted integer array, find three integers can form a triangle
+ * 
+ * http://www.geeksforgeeks.org/find-number-of-triangles-possible/
+ * */
+public class CheckTriangle {
+
+	/**
+	 * a + b > c
+       a + c > b
+       b + c > a
+	 * 
+	 * */
+}
