4b643c34f5d8ea06374255cd43357dab06278200
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-03-02 23:14:35 +0000

    radix sort

---
+++ trunk/leetcode/src/basics/RadixSort.java
@@ -0,0 +1,56 @@
+package basics;
+
+import java.util.ArrayList;
+
+
+/**
+ * 
+ * given an array of 32-bit integers, the array is already sorted according
+to the most significant 28bits, sort the array in O(n)
+
+(just use four radix sort)
+
+the basic idea is to use radix sort
+ * */
+public class RadixSort {
+
+	public void radixSort(int arr[], int maxDigits){
+		int exp = 1;//10^0;
+		for(int i =0; i < maxDigits; i++){
+			//create a number of bucket
+			ArrayList<Integer> bucketList[] = new ArrayList[10];
+			for(int k=0; k < 10; k++){
+				bucketList[k] = new ArrayList<Integer>();
+			}
+			//get that number
+			for(int j =0; j < arr.length; j++){
+				int number = (arr[j]/exp)%10; //put to the right bucket
+				bucketList[number].add(arr[j]);
+			}
+			exp *= 10;
+			int index =0;
+			for(int k=0; k < 10; k++){
+				for(int num: bucketList[k]){
+					arr[index] = num;
+					index++;
+				}
+			}
+			
+			System.out.println("pass: ");
+			for(int in =0; in < arr.length; in++){
+				System.out.print(arr[in] +", ");
+			}
+		}
+
+		System.out.println("Sorted numbers");
+		for(int i =0; i < arr.length; i++){
+			System.out.print(arr[i] +", ");
+		}
+	}
+
+	public static void main(String[] argv){
+		int n = 5;
+		int arr[] = {1,4,2,3,5,10,8};
+		new RadixSort().radixSort(arr, 2);
+	}
+}
