1177cba21b63cbfd61550dea9304d2beec272242
racezhang <racezhang@gmail.com>
2014-03-03 03:55:51 +0000

    trie

---
+++ leetcode/src/basics/Trie.java
@@ -0,0 +1,26 @@
+package basics;
+
+/**
+ * wiki:
+ * http://en.wikipedia.org/wiki/Trie
+ * suffix tree: http://en.wikipedia.org/wiki/Suffix_tree
+ * 
+ * the worst case is: 2n for suffix tree for a fibancci word: 
+ *    0
+ *    01
+ *    010
+ *    01001
+ *    01001010
+ *    0100101001001
+ *    
+ * 
+ * some applications:
+ *  longest common substring
+ *  
+ * 
+ * */
+public class Trie {
+
+	//see: LongestComposedWord
+	//has a trie implementation
+}
