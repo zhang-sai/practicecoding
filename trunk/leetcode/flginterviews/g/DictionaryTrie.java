f91d5d11fb0a65d57a153400ffadfb1da39e9395
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-03-03 00:16:52 +0000

    ditionary trie

---
+++ trunk/leetcode/flginterviews/g/DictionaryTrie.java
@@ -0,0 +1,21 @@
+package g;
+
+/**
+ * a dictionary, and a set of coding string (g5, goo3,...) return all
+ * string from the dictionary that can be matched with the coding string
+ * 
+ * using trie
+ * 
+ * http://www.mitbbs.com/article_t/JobHunting/32634303.html
+ * */
+public class DictionaryTrie {
+
+	
+	/**
+	 * 
+	 *  using trie, each node keep track of the element
+	 * 
+	 * */
+	
+
+}
