7e72c95fdd5fd4d5b44d1888295691baa61a72e9
racezhang <racezhang@gmail.com>
2014-03-03 00:15:00 +0000

    longest word from dictionary

---
+++ leetcode/flginterviews/g/LongestWordFromDict.java
@@ -0,0 +1,13 @@
+package g;
+
+/**
+ * Given a dictionary and a string, find the longest word in the dictionary that is composed by
+ * the letter of that string
+ * */
+
+public class LongestWordFromDict {
+
+	//use a boolean vector to keep track
+	//and then do brute force search
+	
+}
