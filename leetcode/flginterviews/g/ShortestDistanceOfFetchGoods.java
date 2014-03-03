5fe702178f9bdca2be3859e5fe6851dcd42318f8
racezhang <racezhang@gmail.com>
2014-03-03 00:02:07 +0000

    shortest path

---
+++ leetcode/flginterviews/g/ShortestDistanceOfFetchGoods.java
@@ -0,0 +1,15 @@
+package g;
+
+/**
+ * original: http://www.mitbbs.com/article_t/JobHunting/32631467.html
+ * 
+ * a 2-d array, # is good, * is the barrier
+ * 
+ * find a point that to minimize the total distance of fetching all goods
+ * */
+
+public class ShortestDistanceOfFetchGoods {
+
+	//from each good, and perform BFS, and then summarize the shortest path
+	
+}
