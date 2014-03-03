f817dac769e7797fb142ff5f3bb1288ce0b7ef4b
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-03-03 00:06:42 +0000

    crach password

---
+++ trunk/leetcode/flginterviews/g/CrackPassword.java
@@ -0,0 +1,26 @@
+package g;
+
+/**
+ * a six digit password, a function call returns 3 random values for each call.
+ * 
+ * how to crack the password, e.g., google --> it may return goo, oog, gle
+ * 
+ * http://www.mitbbs.com/article_t/JobHunting/32634303.html
+ * */
+
+public class CrackPassword {
+
+	/**
+	 * 
+	 * Maintain the topological order for each result
+	 * 
+	 * g -> o -> o -> g
+	 *  \
+	 *   l -> e
+	 * 
+	 * o->o->g
+	 * 
+	 * 
+	 * */
+	
+}
