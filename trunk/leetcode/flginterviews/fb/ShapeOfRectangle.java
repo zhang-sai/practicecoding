4b038d6c7f1fe29e8f358968646c998c3e1a094d
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-03-03 06:30:39 +0000

    add some test case

--- trunk/leetcode/flginterviews/fb/ShapeOfRectangle.java
+++ trunk/leetcode/flginterviews/fb/ShapeOfRectangle.java
@@ -1,6 +1,7 @@
 package fb;
 
 x
+
 //http://www.mitbbs.com/article_t/JobHunting/32632953.html
 //a set of rectangles, each of which the bottom line is the x-axile
 //please output the overall shape of merging all rectanges
@@ -16,6 +17,22 @@ public class ShapeOfRectangle {
 	 * 
 	 * */
 	
+	
+	public static void main(String[] args) {
+		Rectange r1 = new Rectange(0, 2, 4, 0);
+		Rectange r2 = new Rectange(1, 1, 2, 0);
+		Rectange r3 = new Rectange(2, 1, 3, 0);
+		Rectange r4 = new Rectange(3, 4, 5, 0);
+		Rectange r5 = new Rectange(6, 4, 7, 0);
+		Rectange r6 = new Rectange(6, 2, 8, 0);
+		
+		/**
+		 * results:
+		 * 
+		 * (0, 2) (3, 2) (3, 4), (5, 4) (5, 0)  (6, 0) (6, 4) (7, 2) (8, 2) (8, 0)
+		 * 
+		 * */
+	}
 	
 }
 
