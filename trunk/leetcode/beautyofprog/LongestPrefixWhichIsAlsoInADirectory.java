5465848b28a4bbd9a95685e7d11203719bbd3946
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-03-04 03:45:55 +0000

    longest prefix trie

---
+++ trunk/leetcode/beautyofprog/LongestPrefixWhichIsAlsoInADirectory.java
@@ -0,0 +1,19 @@
+
+//http://www.geeksforgeeks.org/longest-prefix-matching-a-trie-based-solution-in-java/
+public class LongestPrefixWhichIsAlsoInADirectory {
+
+	//use a trie as data structure
+	
+	/**
+	 * Trie over BST
+	 * - looking up for keys are fast, O(length of key), for BST, it is log(n)
+	 * - more space-efficient if it contains lots of short keys
+	 * - balance
+	 * 
+	 * hashtable
+	 * - trie supports ordered iteration
+	 * - tire can have prefix matching
+	 * - faster in average insertion (for hashcode, it needs rehashing, etc)
+	 * - no hash function is needed
+	 * */
+}
