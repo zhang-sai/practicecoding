1054ba3ed8496c8ccc39fd26309e29431aefd89a
racezhang@gmail.com <racezhang@gmail.com@d69c4eb3-d569-ac0e-4e54-5544f97abba7>
2014-03-06 06:28:16 +0000

    fix a problem

--- trunk/leetcode/src/basics/PrimeFactors.java
+++ trunk/leetcode/src/basics/PrimeFactors.java
@@ -14,27 +14,28 @@ public class PrimeFactors {
 	public static List<Integer> primeFactors(int number) {
 		int n = number;
 		List<Integer> factors = new ArrayList<Integer>();
-		for (int i = 2; i <= n/i; i++) { //note it is n/i
-			while (n % i == 0) {
+		for (int i = 2; i <= number/2; i++) {
+			while ( n % i == 0) {
 				factors.add(i);
 				n /= i;
+//				System.out.println("n is: " + n);
 			}
+//			System.out.println("n is: " + n);
+//			System.out.println("i is: " + i);
 		}
 		return factors;
 	}
 	
 	public static void main(String[] args) {
 	    System.out.println("Primefactors of 44");
-	    for (Integer integer : primeFactors(44)) {
-	      System.out.println(integer);
-	    }
-	    System.out.println("Primefactors of 3");
-	    for (Integer integer : primeFactors(3)) {
-	      System.out.println(integer);
-	    }
-	    System.out.println("Primefactors of 32");
-	    for (Integer integer : primeFactors(32)) {
-	      System.out.println(integer);
-	    }
+	    System.out.println(primeFactors(44));
+//	    System.out.println("Primefactors of 3");
+//	    for (Integer integer : primeFactors(3)) {
+//	      System.out.println(integer);
+//	    }
+//	    System.out.println("Primefactors of 32");
+//	    for (Integer integer : primeFactors(32)) {
+//	      System.out.println(integer);
+//	    }
 	  }
 }
